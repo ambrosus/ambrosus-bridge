5ef0c531e7468b5a857efb28e4d310e0c42edb21
kiw <anon@anon.com>
2022-01-14 16:05:50 +0200

    withdraw logic

--- contracts/contracts/AmbBridge.sol
+++ contracts/contracts/AmbBridge.sol
@@ -2,7 +2,12 @@
 pragma solidity 0.8.6;
 
 contract AmbBridge {
+    // event Test(bytes32 indexed withdraws_hash, Withdraw[] withdraws);
+    event newWithdraw(uint event_id, Withdraw[] queue);
+
+
     struct Withdraw {
+        address tokenExtAddress;
         address fromAddress;
         address toAddress;
         uint amount;
@@ -10,18 +15,32 @@ contract AmbBridge {
 
     Withdraw[] queue;
 
-    event Test(bytes32 indexed withdraws_hash, Withdraw[] withdraws);
+    uint lastTimeframeWithActions;
+
+    uint eventId;
 
     constructor() {}
 
 
-    function withdraw(address toAddr, uint amount) public {
-        queue.push(Withdraw(msg.sender, toAddr, amount));
+    function getTimeframe(uint timestamp_) private view returns (uint) {
+        return timestamp_ / uint(4);
+    }
+
+    function withdraw(address tokenAmbAddress, address toAddress, uint amount) public {
+        if (block.timestamp != getTimeframe(lastTimeframeWithActions)) {
+            emit newWithdraw(eventId, queue);
+            delete queue;
+        }
+
+        queue.push(Withdraw(tokenAmbAddress, msg.sender, toAddress, amount));
+        lastTimeframeWithActions = getTimeframe(block.timestamp);
+
+        eventId += 1;
     }
 
 
     function eventTest() public {
-        emit Test(keccak256(abi.encode(queue)), queue);
+//        emit Test(keccak256(abi.encode(queue)), queue);
         delete queue;
     }
 }
