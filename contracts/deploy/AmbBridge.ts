8b2b21936de54ae0180d2e2bc139fa45fb4e4f25
kiwvic <75341499+kiwvic@users.noreply.github.com>
2022-01-20 18:05:38 +0200

    comments for deploy

--- contracts/deploy/AmbBridge.ts
+++ contracts/deploy/AmbBridge.ts
@@ -8,10 +8,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   await hre.deployments.deploy("AmbBridge", {
     contract: "AmbBridge",
     from: owner,
-    args: ["0x295c2707319ad4beca6b5bb4086617fd6f240cfe", "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-      ["0x195c2707319ad4beca6b5bb4086617fd6f240cfe", "0x295c2707319ad4beca6b5bb4086617fd6f240cfe", "0x395c2707319ad4beca6b5bb4086617fd6f240cfe"],
-      ["0x495c2707319ad4beca6b5bb4086617fd6f240cfe", "0x595c2707319ad4beca6b5bb4086617fd6f240cfe", "0x695c2707319ad4beca6b5bb4086617fd6f240cfe"],
-      1000, 14400, 1000],
+    args: [
+      "0x295c2707319ad4beca6b5bb4086617fd6f240cfe", // _sideBridgeAddress
+      "0x295c2707319ad4beca6b5bb4086617fd6f240cfe", // relayAddress
+      ["0x195c2707319ad4beca6b5bb4086617fd6f240cfe", "0x295c2707319ad4beca6b5bb4086617fd6f240cfe", "0x395c2707319ad4beca6b5bb4086617fd6f240cfe"], // tokenThisAddresses
+      ["0x495c2707319ad4beca6b5bb4086617fd6f240cfe", "0x595c2707319ad4beca6b5bb4086617fd6f240cfe", "0x695c2707319ad4beca6b5bb4086617fd6f240cfe"], // tokenSideAddresses
+      1000, // fee
+      14400, // timeframeSeconds
+      1000 // lockTime
+    ],
     log: true,
   });
 
