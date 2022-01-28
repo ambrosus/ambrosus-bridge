455e40f062e504eb70841d810eec7736732f3e69
nacknime-official <nacknime.official@gmail.com>
2022-01-28 18:19:49 +0200

    Encode PoW block

--- relay/networks/eth/encode_block.go
+++ relay/networks/eth/encode_block.go
@@ -1,20 +1,22 @@
 package eth
 
 import (
-	"encoding/hex"
-	"fmt"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/rlp"
 	"relay/contracts"
 	"relay/helpers"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
-func EncodeBlock(header *types.Header, isEventBlock bool) contracts.CheckPoWBlockPoW {
+func EncodeBlock(header *types.Header, isEventBlock bool) *contracts.CheckPoWBlockPoW {
 	// split rlp encoded header (bytes) by
 	// - receiptHash (for event block) / parentHash (for safety block)
 	// - Difficulty (for PoW)
 
-	rlpHeader, _ := rlp.EncodeToBytes(header)
+	rlpHeader, err := rlp.EncodeToBytes(header)
+	if err != nil {
+		panic(err)
+	}
 
 	splitEls := make([][]byte, 2)
 	if isEventBlock {
@@ -23,19 +25,14 @@ func EncodeBlock(header *types.Header, isEventBlock bool) contracts.CheckPoWBloc
 		splitEls[0] = header.ParentHash.Bytes()
 	}
 
-	if true {
-		timeBytes, _ := hex.DecodeString(fmt.Sprintf("%x", header.Time))
-		splitEls[1] = timeBytes
-	} else {
-		splitEls[1] = header.Difficulty.Bytes()
-	}
+	splitEls[1] = header.Difficulty.Bytes()
 
 	splitted, err := helpers.BytesSplit(rlpHeader, splitEls)
 	if err != nil {
 		panic(err)
 	}
 
-	return contracts.CheckPoWBlockPoW{
+	return &contracts.CheckPoWBlockPoW{
 		P1:                    splitted[0],
 		PrevHashOrReceiptRoot: helpers.BytesToBytes32(splitEls[0]),
 		P2:                    splitted[1],
