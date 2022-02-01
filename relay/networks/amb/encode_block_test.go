34862cbb57ef8b0f81cbd2f366bf5c433f78bac5
svinerus <svinerus@gmail.com>
2022-02-01 22:47:11 +0200

    Simplify encode_block_test.go

--- relay/networks/amb/encode_block_test.go
+++ relay/networks/amb/encode_block_test.go
@@ -2,6 +2,7 @@ package amb
 
 import (
 	"math/big"
+	"relay/helpers"
 	"relay/receipts_proof/mytrie"
 	"testing"
 
@@ -14,28 +15,24 @@ func Test(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	encodedBlock := EncodeBlock(h, true)
-	rlpEncodedBlock := append(encodedBlock.P1, encodedBlock.PrevHashOrReceiptRoot[:]...)
-	rlpEncodedBlock = append(rlpEncodedBlock, encodedBlock.P2...)
-	rlpEncodedBlock = append(rlpEncodedBlock, encodedBlock.Timestamp...)
-	rlpEncodedBlock = append(rlpEncodedBlock, encodedBlock.P3...)
+	encodedBlockPoA := EncodeBlock(h, true)
 
-	// with seal
-	rlpEncodedBlockWithSeal := append(encodedBlock.P0Seal, rlpEncodedBlock...)
-	rlpEncodedBlockWithSeal = append(rlpEncodedBlockWithSeal, encodedBlock.S1...)
-	rlpEncodedBlockWithSeal = append(rlpEncodedBlockWithSeal, encodedBlock.Signature...)
-	rlpEncodedBlockWithSeal = append(rlpEncodedBlockWithSeal, encodedBlock.S2...)
-	encodedBlockHashWithSeal := common.BytesToHash(mytrie.Hash(rlpEncodedBlockWithSeal))
-	headerHashWithSeal := h.Hash(true)
-	if encodedBlockHashWithSeal != headerHashWithSeal {
-		t.Fatalf("Header hash with seal %s != encodedBlock hash with seal %s", headerHashWithSeal, encodedBlockHashWithSeal)
-	}
+	rlpBase := helpers.BytesConcat(encodedBlockPoA.P1, encodedBlockPoA.PrevHashOrReceiptRoot[:], encodedBlockPoA.P2, encodedBlockPoA.Timestamp, encodedBlockPoA.P3)
 
 	// without seal
-	rlpEncodedBlockWithoutSeal := append(encodedBlock.P0Bare, rlpEncodedBlock...)
-	encodedBlockHashWithoutSeal := common.BytesToHash(mytrie.Hash(rlpEncodedBlockWithoutSeal))
-	headerHashWithoutSeal := h.Hash(false)
-	if encodedBlockHashWithoutSeal != headerHashWithoutSeal {
-		t.Fatalf("Header hash without seal %s != encodedBlock hash without seal %s", headerHashWithSeal, encodedBlockHashWithSeal)
+	rlpWithoutSeal := helpers.BytesConcat(encodedBlockPoA.P0Bare, rlpBase)
+	hashWithoutSeal := common.BytesToHash(mytrie.Hash(rlpWithoutSeal))
+
+	if hashWithoutSeal != h.Hash(false) {
+		t.Fatalf("wrong bare hash")
 	}
+
+	// with seal
+	rlpWithSeal := helpers.BytesConcat(encodedBlockPoA.P0Seal, rlpBase, encodedBlockPoA.S1, encodedBlockPoA.Signature, encodedBlockPoA.S2)
+	hashWithSeal := common.BytesToHash(mytrie.Hash(rlpWithSeal))
+
+	if hashWithSeal != h.Hash(true) {
+		t.Fatalf("wrong seal hash")
+	}
+
 }
