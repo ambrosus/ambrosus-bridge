1b98ffb0f3ce43399b9393deaa29100f4109107c
nacknime-official <nacknime.official@gmail.com>
2022-02-09 14:18:17 +0200

    add ChainId to amb config
    30741 is for the developer network

--- relay/config/config.go
+++ relay/config/config.go
@@ -25,6 +25,7 @@ var Config = map[string]*Bridge{
 		ContractAddress: ethcommon.HexToAddress(""),
 		PrivateKey:      ParsePK("34d8e83fca265e9ab5bcc1094fa64e98692375bf8980d066a9edcf4953f0f2f5"),
 		SafetyBlocks:    10,
+		ChainID:         big.NewInt(30741),
 	},
 	"eth": {
 		Url:             "https://rinkeby.infura.io/v3/01117e8ede8e4f36801a6a838b24f36c",
