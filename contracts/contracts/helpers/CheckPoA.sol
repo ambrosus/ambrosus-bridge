f7adf140353c31b1655a051e6b00b34eebd58be9
svinerus <svinerus@gmail.com>
2022-02-13 00:59:04 +0200

    Update PoA check

--- contracts/contracts/helpers/CheckPoA.sol
+++ contracts/contracts/helpers/CheckPoA.sol
@@ -5,58 +5,90 @@ import "./CommonStructs.sol";
 
 
 contract CheckPoA {
-
     struct BlockPoA {
         bytes p0_seal;
         bytes p0_bare;
 
         bytes p1;
-        bytes32 prevHashOrReceiptRoot;  // receipt for main block, prevHash for safety blocks
+        bytes32 parent_hash;
         bytes p2;
-        bytes timestamp;
+        bytes32 receipts_hash;
         bytes p3;
 
         bytes s1;
-        bytes signature;
+        bytes step;
         bytes s2;
+        bytes signature;
+
+        uint type_;
     }
 
 
-    function CheckPoA_(BlockPoA[] memory blocks, CommonStructs.Transfer[] memory events, bytes[] memory proof) public {
-//        bytes32 hash = calcReceiptsRoot(proof, abi.encode(events));
+    struct ValidatorSet_Event {
+        bytes[] receipt_proof;
+        address delta_address;
+        uint96 delta_index;  // 12байт шоб спаковать с 20байт адресом
+    }
+
+
+    struct Transfer_Event {
+        bytes[] receipt_proof;
+        uint event_id;
+        CommonStructs.Transfer[] transfers;
+    }
+
+
+    function CheckPoA_(BlockPoA[] memory blocks, Transfer_Event memory transfer, ValidatorSet_Event[] memory vs_changes) public {
+        uint safetyChainLength;
 
         for (uint i = 0; i < blocks.length; i++) {
-//            require(blocks[i].prevHashOrReceiptRoot == hash, "prevHash or receiptRoot wrong");
+            // check signature, calc hash
+            bytes32 block_hash = CheckBlock(blocks[i]);
 
-            bytes memory rlp = abi.encodePacked(blocks[i].p1, blocks[i].prevHashOrReceiptRoot, blocks[i].p2, blocks[i].timestamp, blocks[i].p3);
+            if (blocks[i].type_ == -3) {  // end of safety chain
+                require(safetyChainLength >= minSafetyBlocks, "safety chain too short");
+                safetyChainLength = 0;
+            } else {
+                require(block_hash == blocks[i+1].parent_hash, "wrong parent hash");
+                safetyChainLength++;
+            }
 
-            // hash without seal for signature check
-            bytes32 bare_hash = keccak256(abi.encodePacked(blocks[i].p0_bare, rlp));
-            address validator = GetValidator(bytesToUint(blocks[i].timestamp));
-            CheckSignature(validator, bare_hash, blocks[i].signature);
+            if (block.type_ >= 0) { // validator set change event
+                // todo check vs event
+            }
+            else if (block.type_ == -1) { // transfer event
+                // todo check transfers event
+            }
 
-            // hash with seal, for prev_hash check
-//            hash = keccak256(abi.encodePacked(blocks[i].p0_seal, rlp, blocks[i].s1, blocks[i].signature, blocks[i].s2));
         }
+
+    }
+
+    function CheckBlock(BlockPoA memory block) internal view returns (bytes32) {
+        bytes memory common_rlp = abi.encodePacked(blocks[i].p1, block.parent_hash, blocks[i].p2, blocks[i].receipts_hash, blocks[i].p3);
+
+        // hash without seal for signature check
+        bytes32 bare_hash = keccak256(abi.encodePacked(blocks[i].p0_bare, common_rlp));
+        address validator = GetValidator(bytesToUint(blocks[i].step));
+        CheckSignature(validator, bare_hash, blocks[i].signature);  // revert if wrong
+
+        // hash with seal, for prev_hash check
+        return keccak256(abi.encodePacked(blocks[i].p0_seal, common_rlp, blocks[i].s1, blocks[i].step, blocks[i].s2, blocks[i].signature));
+
     }
 
 
-    function GetValidator(uint timestamp) internal view returns (address) {
-        // todo
-        return address(this);
+
+    function CheckReceiptsProof(bytes[] memory proof, address eventContractAddress, bytes topic, bytes data, bytes32 receciptsRoot) public {
+        require(calcReceiptsRoot(proof, eventContractAddress, topic,  data) == receiptsRoot, "Failed to verify receipts proof");
     }
 
-
-
-    function CheckReceiptsProof(bytes[] memory proof, bytes memory eventToSearch, bytes32 receiptsRoot) public {
-        require(calcReceiptsRoot(proof, eventToSearch) == receiptsRoot, "Failed to verify receipts proof");
-    }
-
-    function calcReceiptsRoot(bytes[] memory proof, bytes memory eventToSearch) public view returns (bytes32){
-        bytes32 el = keccak256(abi.encodePacked(proof[0], eventToSearch, proof[1]));
+    // check readme for focs
+    function calcReceiptsRoot(bytes[] memory proof, address eventContractAddress, bytes topic, bytes data) public view returns (bytes32){
+        bytes32 el = keccak256(abi.encodePacked(proof[0], eventContractAddress, proof[1], topic, proof[2], data, proof[3]));
         bytes memory s;
 
-        for (uint i = 2; i < proof.length - 1; i += 2) {
+        for (uint i = 4; i < proof.length; i += 2) {
             s = abi.encodePacked(proof[i], el, proof[i + 1]);
             el = (s.length > 32) ? keccak256(s) : bytes32(s);
         }
@@ -64,6 +96,10 @@ contract CheckPoA {
         return el;
     }
 
+    function GetValidator(uint step) internal view returns (address) {
+        // todo
+        return address(this);
+    }
 
     function CheckSignature(address signer, bytes32 message, bytes memory signature) internal view {
         bytes32 r;
