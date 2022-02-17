6fd4b62a9b6b77750ff376a73fbeb86fce7f079a
V1def <vaniya8871@gmail.com>
2022-02-17 17:22:23 +0200

    Added generating epoch data

---
+++ relay/pkg/ethash/epoch.go
@@ -0,0 +1,30 @@
+package ethash
+
+import (
+	"fmt"
+	"math/big"
+)
+
+const (
+	epochLength uint64 = 30000
+	maxEpoch    uint64 = 2048
+)
+
+type EpochData struct {
+	Epoch                   *big.Int
+	FullSizeIn128Resolution *big.Int
+	BranchDepth             *big.Int
+	MerkleNodes             []*big.Int
+}
+
+func GenerateEpochData(epoch uint64) *EpochData {
+	fullSize := SizeDAG(epoch * epochLength)
+	fullSizeIn128Resolution := fullSize / 128
+	branchDepth := len(fmt.Sprintf("%b", fullSizeIn128Resolution-1))
+
+	return &EpochData{
+		Epoch:                   big.NewInt(int64(epoch)),
+		FullSizeIn128Resolution: big.NewInt(int64(fullSizeIn128Resolution)),
+		BranchDepth:             big.NewInt(int64(branchDepth - 10)),
+	}
+}
