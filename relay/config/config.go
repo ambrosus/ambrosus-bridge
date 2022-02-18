291c2572d697c31374bf18932055a3d5a24b1256
nacknime-official <nacknime.official@gmail.com>
2022-02-18 10:33:13 +0200

    remove 'ChainID' field from config

--- relay/config/config.go
+++ relay/config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"crypto/ecdsa"
 	"encoding/hex"
-	"math/big"
 
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -15,7 +14,6 @@ type Bridge struct {
 	VSContractAddress ethcommon.Address
 	PrivateKey        *ecdsa.PrivateKey
 	SafetyBlocks      uint64
-	ChainID           *big.Int
 }
 
 // todo load from json
@@ -33,7 +31,6 @@ var Config = map[string]*Bridge{
 		ContractAddress: ethcommon.HexToAddress(""),
 		PrivateKey:      ParsePK("34d8e83fca265e9ab5bcc1094fa64e98692375bf8980d066a9edcf4953f0f2f5"),
 		SafetyBlocks:    10,
-		ChainID:         big.NewInt(1),
 	},
 }
 
