024a0d677773bf9c999c8e51a3667725436dff52
V1def <vaniya8871@gmail.com>
2022-02-22 21:53:43 +0200

    Refactoring generating testing data

--- relay/cmd/dump-test-data/main.go
+++ relay/cmd/dump-test-data/main.go
@@ -19,43 +19,54 @@ import (
 )
 
 func main() {
+	// Ambrosus configgurations for generating testing data.
 	ambCfg := networkConfig{
 		Name: "amb",
 		Url:  "https://network.ambrosus-dev.io",
 		Tx:   common.HexToHash("0xfe802d29486f3648fd082ecad8c8455aa751b18f36be7cff3cfd65245253233a"),
 	}
+	// Ethereum configgurations for generating testing data.
 	ethCfg := networkConfig{
-		Name: "eth",
-		Url:  "https://rinkeby.infura.io/v3/01117e8ede8e4f36801a6a838b24f36c",
-		Tx:   common.HexToHash("0x3ccead6d9ce5dba1add833c4b2631ad2066aad6d2ef7bfe278eb259a7355d9e2"),
+		Name:  "eth",
+		Url:   "https://rinkeby.infura.io/v3/01117e8ede8e4f36801a6a838b24f36c",
+		Tx:    common.HexToHash("0x3ccead6d9ce5dba1add833c4b2631ad2066aad6d2ef7bfe278eb259a7355d9e2"),
+		Block: common.HexToHash("0xc4ca0efd5d528d67691abd9e10e9d4ca570f16235779e1f314b036caa5b455a1"),
 	}
 
+	// Generating ambrosus testing data.
 	if err := generateNetworkTestData(ambCfg); err != nil {
 		log.Error().Err(err).Msgf("error generating '%s' test data", ambCfg.Name)
 	}
 
+	// Generating ethereum testing data.
 	if err := generateNetworkTestData(ethCfg); err != nil {
 		log.Error().Err(err).Msgf("error generating '%s' test data", ethCfg.Name)
 	}
 
+	// Generating dataset testing data.
 	if err := generateDatasetTestData(ethCfg); err != nil {
 		log.Error().Err(err).Msg("error generating dataset test data")
 	}
 }
 
+// Network testing data.
 type networkData struct {
 	Log      *types.Log
 	Header   *types.Header
 	Receipts []*types.Receipt
 }
 
+// Configurations for generating network testing data.
 type networkConfig struct {
-	Name string
-	Url  string
-	Tx   common.Hash
+	Name  string      // Network name.
+	Url   string      // Network URL.
+	Tx    common.Hash // Network transaction hash.
+	Block common.Hash // Network block hash.
 }
 
+// Generating network testing data.
 func generateNetworkTestData(cfg networkConfig) error {
+	// Creating a new ambrosus bridge client.
 	bridge, err := amb.New(&config.Bridge{Url: cfg.Url})
 	if err != nil {
 		return err
@@ -87,6 +98,7 @@ func generateNetworkTestData(cfg networkConfig) error {
 	return writeToJSONFile(data, fmt.Sprintf("./receipts_proof/fixtures/%s-data.json", cfg.Name))
 }
 
+// Dataset testing data.
 type datasetData struct {
 	BlockHash        common.Hash
 	EpochData        *ethash.EpochData
@@ -94,20 +106,22 @@ type datasetData struct {
 	WitnessForLookup []*big.Int
 }
 
+// Generating dataset testing data.
 func generateDatasetTestData(cfg networkConfig) error {
+	// Creating a new ethereum bridge client.
 	bridge, err := eth.New(&config.Bridge{Url: "https://mainnet.infura.io/v3/ab050ca98686478e9e9b06dfc3b2f069"})
 	if err != nil {
 		return err
 	}
 	defer bridge.Client.Close()
 
-	blockHash := common.HexToHash("0xe9bff194462f02de6ba677cba76657b8eed353cd0bd95785faace53f20fb705f")
-
-	block, err := bridge.Client.BlockByHash(context.Background(), blockHash)
+	// Getting block by hash.
+	block, err := bridge.Client.BlockByHash(context.Background(), cfg.Block)
 	if err != nil {
 		log.Error().Err(err).Msg("block not getting")
 	}
 
+	// Encode block header without nonce.
 	blockHeaderWithoutNonce, err := eth.EncodeHeaderWithoutNonceToRLP(block.Header())
 	if err != nil {
 		log.Error().Err(err).Msg("block header not encode")
@@ -118,22 +132,26 @@ func generateDatasetTestData(cfg networkConfig) error {
 	var blockHeaderHashWithoutNonceLength32 [32]byte
 	copy(blockHeaderHashWithoutNonceLength32[:], blockHeaderHashWithoutNonce)
 
+	// Creating a new meta data block.
 	blockMetaData := ethash.NewBlockMetaData(
 		block.Header().Number.Uint64(), block.Header().Nonce.Uint64(),
 		blockHeaderHashWithoutNonceLength32,
 	)
 
+	// Creating dataset lookup.
 	dataSetLookUp := blockMetaData.DAGElementArray()
+	// Creating witness for lookup
 	witnessForLookup := blockMetaData.DAGProofArray()
 
 	epoch := block.Header().Number.Uint64() / 30000
+	// Generating epoch data.
 	epochData, err := ethash.GenerateEpochData(epoch)
 	if err != nil {
 		return err
 	}
 
 	data := datasetData{
-		BlockHash:        blockHash,
+		BlockHash:        cfg.Block,
 		EpochData:        epochData,
 		DataSetLookUp:    dataSetLookUp,
 		WitnessForLookup: witnessForLookup,
