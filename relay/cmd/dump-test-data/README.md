478437b70cde089a64a5508cd154cefa876b2b31
V1def <vaniya8871@gmail.com>
2022-02-23 22:00:00 +0200

    Update generating pow and poa test data

--- relay/cmd/dump-test-data/README.md
+++ relay/cmd/dump-test-data/README.md
@@ -1,6 +1,24 @@
 # Dump Test Data
 
 ## Usage:
+
+go run cmd/dump-test-data/main.go [comamnd] [arg]
+
+### Comamnds:
++ receipts-proof [network]
++ pow-block [block number]
++ poa-block [block number]
+
+Networks:
++ amb
++ eth
+
+## Examples:
+Generating ambrosus testing data for receipts proof:
 ```sh
-go run cmd/dump-test-data/main.go
+go run cmd/dump-test-data/main.go receipts-proof amb
+```
+Encoding ethereum PoW block:
+```sh
+go run cmd/dump-test-data/main.go pow-block 14257704
 ```
--- relay/cmd/dump-test-data/main.go
+++ relay/cmd/dump-test-data/main.go
@@ -3,49 +3,58 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"math/big"
+	"os"
+	"strconv"
 
 	"github.com/ambrosus/ambrosus-bridge/relay/config"
 	"github.com/ambrosus/ambrosus-bridge/relay/networks/amb"
 	"github.com/ambrosus/ambrosus-bridge/relay/networks/eth"
-	"github.com/ambrosus/ambrosus-bridge/relay/pkg/ethash"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/rs/zerolog/log"
 )
 
-func main() {
+var (
 	// Ambrosus configgurations for generating testing data.
-	ambCfg := networkConfig{
+	ambCfg = networkConfig{
 		Name: "amb",
 		Url:  "https://network.ambrosus-dev.io",
 		Tx:   common.HexToHash("0xfe802d29486f3648fd082ecad8c8455aa751b18f36be7cff3cfd65245253233a"),
 	}
 	// Ethereum configgurations for generating testing data.
-	ethCfg := networkConfig{
-		Name:  "eth",
-		Url:   "https://rinkeby.infura.io/v3/01117e8ede8e4f36801a6a838b24f36c",
-		Tx:    common.HexToHash("0x3ccead6d9ce5dba1add833c4b2631ad2066aad6d2ef7bfe278eb259a7355d9e2"),
-		Block: common.HexToHash("0xc4ca0efd5d528d67691abd9e10e9d4ca570f16235779e1f314b036caa5b455a1"),
+	ethCfg = networkConfig{
+		Name: "eth",
+		Url:  "https://rinkeby.infura.io/v3/01117e8ede8e4f36801a6a838b24f36c",
+		Tx:   common.HexToHash("0x3ccead6d9ce5dba1add833c4b2631ad2066aad6d2ef7bfe278eb259a7355d9e2"),
 	}
 
-	// Generating ambrosus testing data.
-	if err := generateNetworkTestData(ambCfg); err != nil {
-		log.Error().Err(err).Msgf("error generating '%s' test data", ambCfg.Name)
-	}
+	ErrArgNotFound = errors.New("error argument not found")
+)
 
-	// Generating ethereum testing data.
-	if err := generateNetworkTestData(ethCfg); err != nil {
-		log.Error().Err(err).Msgf("error generating '%s' test data", ethCfg.Name)
-	}
+func main() {
+	switch os.Args[1] {
+	case "receipts-proof":
+		receiptsProofInput(os.Args[2])
+	case "pow-block":
+		// Encoding PoW block.
+		if err := encodePoWBlock(ethCfg); err != nil {
+			log.Fatal().Err(err).Msg("error encoding pow block")
+		}
 
-	// Generating dataset testing data.
-	if err := generateDatasetTestData(ethCfg); err != nil {
-		log.Error().Err(err).Msg("error generating dataset test data")
+		return
+	case "poa-block":
+		// Encoding PoA block.
+		if err := encodePoABlock(ambCfg); err != nil {
+			log.Fatal().Err(err).Msg("error encoding poa block")
+		}
+
+		return
+	default:
+		log.Warn().Msg("command not found")
 	}
 }
 
@@ -58,14 +67,37 @@ type networkData struct {
 
 // Configurations for generating network testing data.
 type networkConfig struct {
-	Name  string      // Network name.
-	Url   string      // Network URL.
-	Tx    common.Hash // Network transaction hash.
-	Block common.Hash // Network block hash.
+	Name string      // Network name.
+	Url  string      // Network URL.
+	Tx   common.Hash // Network transaction hash.
 }
 
-// Generating network testing data.
-func generateNetworkTestData(cfg networkConfig) error {
+// Receipt proof input.
+func receiptsProofInput(args string) {
+	switch args {
+	case "amb":
+		// Generating ambrosus testing data for receipts proof.
+		if err := dataForReceiptProof(ambCfg); err != nil {
+			log.Fatal().Err(err).Msgf("error generating '%s' test data for receipts proof", ambCfg.Name)
+		}
+
+		return
+	case "eth":
+		// Generating ethereum testing data for receitps proof.
+		if err := dataForReceiptProof(ethCfg); err != nil {
+			log.Fatal().Err(err).Msgf("error generating '%s' test data for receipts proof", ethCfg.Name)
+		}
+
+		return
+	default:
+		log.Warn().Msg("Specify the network tag!")
+	}
+}
+
+// Generating network testing data for receipts proof.
+func dataForReceiptProof(cfg networkConfig) error {
+	log.Info().Msgf("Generating data for %s receipts proof..", cfg.Name)
+
 	// Creating a new ambrosus bridge client.
 	bridge, err := amb.New(&config.AMBConfig{Network: config.Network{URL: cfg.Url}})
 	if err != nil {
@@ -98,16 +130,10 @@ func generateNetworkTestData(cfg networkConfig) error {
 	return writeToJSONFile(data, fmt.Sprintf("./receipts_proof/fixtures/%s-data.json", cfg.Name))
 }
 
-// Dataset testing data.
-type datasetData struct {
-	BlockHash        common.Hash
-	EpochData        *ethash.EpochData
-	DataSetLookUp    []*big.Int
-	WitnessForLookup []*big.Int
-}
+// Encoding PoW block.
+func encodePoWBlock(cfg networkConfig) error {
+	log.Info().Msg("Encoding pow block...")
 
-// Generating dataset testing data.
-func generateDatasetTestData(cfg networkConfig) error {
 	// Creating a new ethereum bridge client.
 	bridge, err := eth.New(&config.ETHConfig{
 		Network: config.Network{URL: "https://mainnet.infura.io/v3/ab050ca98686478e9e9b06dfc3b2f069"},
@@ -117,49 +143,62 @@ func generateDatasetTestData(cfg networkConfig) error {
 	}
 	defer bridge.Client.Close()
 
-	// Getting block by hash.
-	block, err := bridge.Client.BlockByHash(context.Background(), cfg.Block)
-	if err != nil {
-		log.Error().Err(err).Msg("block not getting")
+	if len(os.Args) < 3 {
+		return ErrArgNotFound
 	}
 
-	// Encode block header without nonce.
-	blockHeaderWithoutNonce, err := eth.EncodeHeaderWithoutNonceToRLP(block.Header())
-	if err != nil {
-		log.Error().Err(err).Msg("block header not encode")
-	}
-
-	blockHeaderHashWithoutNonce := crypto.Keccak256(blockHeaderWithoutNonce)
-
-	var blockHeaderHashWithoutNonceLength32 [32]byte
-	copy(blockHeaderHashWithoutNonceLength32[:], blockHeaderHashWithoutNonce)
-
-	// Creating a new meta data block.
-	blockMetaData := ethash.NewBlockMetaData(
-		block.Header().Number.Uint64(), block.Header().Nonce.Uint64(),
-		blockHeaderHashWithoutNonceLength32,
-	)
-
-	// Creating dataset lookup.
-	dataSetLookUp := blockMetaData.DAGElementArray()
-	// Creating witness for lookup
-	witnessForLookup := blockMetaData.DAGProofArray()
-
-	epoch := block.Header().Number.Uint64() / 30000
-	// Generating epoch data.
-	epochData, err := ethash.GenerateEpochData(epoch)
+	number, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		return err
 	}
 
-	data := datasetData{
-		BlockHash:        cfg.Block,
-		EpochData:        epochData,
-		DataSetLookUp:    dataSetLookUp,
-		WitnessForLookup: witnessForLookup,
+	// Getting block by hash.
+	block, err := bridge.Client.BlockByNumber(context.Background(), big.NewInt(int64(number)))
+	if err != nil {
+		return err
 	}
 
-	return writeToJSONFile(data, fmt.Sprintf("./assets/testdata/epoch-%d.json", epoch))
+	data, err := eth.EncodeBlock(block.Header(), true)
+	if err != nil {
+		return err
+	}
+
+	return writeToJSONFile(data, fmt.Sprintf("./assets/testdata/BlockPoW-%d.json", block.Header().Number.Uint64()))
+}
+
+// Encoding PoA block.
+func encodePoABlock(cfg networkConfig) error {
+	log.Info().Msg("Encoding poa block...")
+
+	// Creating a new ambrosus bridge client.
+	bridge, err := amb.New(&config.AMBConfig{
+		Network: config.Network{URL: "https://network.ambrosus.io"},
+	})
+	if err != nil {
+		return err
+	}
+	defer bridge.Client.Close()
+
+	if len(os.Args) < 3 {
+		return ErrArgNotFound
+	}
+
+	number, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		return err
+	}
+
+	header, err := bridge.HeaderByNumber(big.NewInt(int64(number)))
+	if err != nil {
+		return err
+	}
+
+	data, err := amb.EncodeBlock(header)
+	if err != nil {
+		return err
+	}
+
+	return writeToJSONFile(data, fmt.Sprintf("./assets/testdata/BlockPoA-%d.json", number))
 }
 
 // Wrire data to json file.
