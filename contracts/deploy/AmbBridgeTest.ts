bf752b4976db8486c00593c4e813e44f0e80d7b9
kiwvic <75341499+kiwvic@users.noreply.github.com>
2022-03-08 21:04:10 +0200

    add tests for lock/unlock, Ethash

---
+++ contracts/deploy/AmbBridgeTest.ts
@@ -0,0 +1,26 @@
+import {HardhatRuntimeEnvironment} from "hardhat/types";
+import {DeployFunction} from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (hre.network.name !== 'hardhat' && hre.network.name !== 'amb') return
+  const {owner} = await hre.getNamedAccounts();
+  await hre.deployments.deploy("AmbBridgeTest", {
+    from: owner,
+    args: [
+      "0x295c2707319ad4beca6b5bb4086617fd6f240cfe", // _sideBridgeAddress
+      "0x295c2707319ad4beca6b5bb4086617fd6f240cfe", // relayAddress
+      ["0x195c2707319ad4beca6b5bb4086617fd6f240cfe", "0x295c2707319ad4beca6b5bb4086617fd6f240cfe", "0x395c2707319ad4beca6b5bb4086617fd6f240cfe"], // tokenThisAddresses
+      ["0x495c2707319ad4beca6b5bb4086617fd6f240cfe", "0x595c2707319ad4beca6b5bb4086617fd6f240cfe", "0x695c2707319ad4beca6b5bb4086617fd6f240cfe"], // tokenSideAddresses
+      1000, // fee
+      14400, // timeframeSeconds
+      1000, // lockTime
+      10 // minSafetyBlocks
+    ],
+    log: true,
+  });
+};
+
+
+
+export default func;
+func.tags = ["ambbridgetest"];
