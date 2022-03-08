88af78856e78519484f6b23900cd6568924ad683
nacknime-official <nacknime.official@gmail.com>
2022-03-08 18:44:27 +0200

    extract getting contract balance into pkg/ethereum/contract.go

--- relay/pkg/metric/ethereum.go
+++ relay/pkg/metric/ethereum.go
@@ -1,22 +1,18 @@
 package metric
 
 import (
-	"context"
-	"math/big"
-
+	"github.com/ambrosus/ambrosus-bridge/relay/pkg/ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/params"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 func SetContractBalance(gauge prometheus.Gauge, client *ethclient.Client, contractAddress common.Address) error {
-	balance, err := client.BalanceAt(context.Background(), contractAddress, nil)
+	balance, err := ethereum.GetBalanceGWei(client, contractAddress)
 	if err != nil {
 		return err
 	}
-	balanceGWei := new(big.Float).Quo(new(big.Float).SetInt(balance), big.NewFloat(params.GWei))
-	balanceFloat64, _ := balanceGWei.Float64()
-	gauge.Set(balanceFloat64)
+
+	gauge.Set(balance)
 	return nil
 }
