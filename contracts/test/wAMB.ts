dacdb4954f85c9a6775d508b81e4fe4ac737dfbb
kiwvic <75341499+kiwvic@users.noreply.github.com>
2022-03-31 20:09:56 +0300

    add wAMB

---
+++ contracts/test/wAMB.ts
@@ -0,0 +1,40 @@
+import { deployments, ethers, getNamedAccounts } from "hardhat";
+import type { Contract, Signer } from "ethers";
+
+import chai from "chai";
+
+chai.should();
+export const expect = chai.expect;
+
+describe("wAMB", () => {
+    let ownerS: Signer;
+    let owner: string;
+    let user: string;
+    let userS: Signer;
+
+    let wAMB: Contract;
+
+    const val = 1000;
+
+    before(async () => {
+        await deployments.fixture(["wamb"]); // reset contracts state
+        ({ owner, user } = await getNamedAccounts());
+        ownerS = await ethers.getSigner(owner);
+        userS = await ethers.getSigner(user);
+
+        wAMB = await ethers.getContract("wAMB", ownerS);
+    });
+
+    describe("wrap/unwrap", () => {
+        it("Test wrap", async () => {
+            await wAMB.connect(userS).wrap({value: val});
+            expect(await wAMB.balanceOf(user)).eq(val);
+        });
+
+        it("Test unwrap", async () => {
+            await expect(
+                () => wAMB.connect(userS).unwrap(val)
+            ).to.changeEtherBalance(userS, 1000);
+        });
+    });
+});
