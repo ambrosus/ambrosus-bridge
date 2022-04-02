d060456226c903a2facdee6ee65ca399817a3ffa
kiwvic <75341499+kiwvic@users.noreply.github.com>
2022-04-02 23:33:03 +0300

    move test-contracts deploy to another dir

--- contracts/deploy/for_tests/CommonBridge.ts
+++ contracts/deploy/for_tests/CommonBridge.ts
@@ -2,7 +2,7 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  if (hre.network.name !== "hardhat" && hre.network.name !== "amb") return;
+  if (hre.network.name !== "hardhat") return;
   const { owner } = await hre.getNamedAccounts();
   await hre.deployments.deploy("CommonBridge", {
     from: owner,
