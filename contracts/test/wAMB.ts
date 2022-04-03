a5edf648b2dd85a86975609cc4fec9120b3b595f
svinerus <svinerus@gmail.com>
2022-04-03 17:59:22 +0300

    Use only 1 user in wAMB test

--- contracts/test/wAMB.ts
+++ contracts/test/wAMB.ts
@@ -7,34 +7,31 @@ chai.should();
 export const expect = chai.expect;
 
 describe("wAMB", () => {
-    let ownerS: Signer;
-    let owner: string;
-    let user: string;
     let userS: Signer;
+    let user: string;
 
     let wAMB: Contract;
 
     const val = 1000;
 
     before(async () => {
-        await deployments.fixture(["for_tests"]); // reset contracts state
-        ({ owner, user } = await getNamedAccounts());
-        ownerS = await ethers.getSigner(owner);
+        await deployments.fixture(["for_tests"]);
+        ({ user } = await getNamedAccounts());
         userS = await ethers.getSigner(user);
 
-        wAMB = await ethers.getContract("wAMB", ownerS);
+        wAMB = await ethers.getContract("wAMB", userS);
     });
 
     describe("wrap/unwrap", () => {
         it("Test wrap", async () => {
-            await wAMB.connect(userS).wrap({value: val});
+            await wAMB.wrap({value: val});
             expect(await wAMB.balanceOf(user)).eq(val);
         });
 
         it("Test unwrap", async () => {
             await expect(
-                () => wAMB.connect(userS).unwrap(val)
-            ).to.changeEtherBalance(userS, 1000);
+                () => wAMB.unwrap(val)
+            ).to.changeEtherBalance(userS, val);
         });
     });
 });
