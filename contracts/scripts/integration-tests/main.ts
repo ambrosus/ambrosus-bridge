f3c973123745373aee543162c017c333c2040434
kiwvic <75341499+kiwvic@users.noreply.github.com>
2022-04-03 21:06:42 +0300

    add amb to eth test (#132)
    * add amb to eth test
    
    * add withdraw event check in tests
    
    * console.log on start Net->Net
    
    Co-authored-by: SvineruS <37367870+SvineruS@users.noreply.github.com>

--- contracts/scripts/integration-tests/main.ts
+++ contracts/scripts/integration-tests/main.ts
@@ -6,7 +6,6 @@ import mockTokens from "./mockTokensAddresses.json";
 import {Contract, providers} from "ethers";
 
 
-
 chai.should();
 const expect = chai.expect;
 
@@ -26,7 +25,6 @@ describe("Integration tests", function () {
     ethBridge = await ethers.getContractAt("EthBridge", bridges.ethAddress, ethSigner);
     ambToken = await ethers.getContractAt("MockERC20", mockTokens.ambErc20Address, ambSigner);
     ethToken = await ethers.getContractAt("MockERC20", mockTokens.ethErc20Address, ethSigner);
-
     // setup relay
     console.log("Setup relay");
     const relayRole = await ethBridge.ADMIN_ROLE();
@@ -44,14 +42,11 @@ describe("Integration tests", function () {
 
     await w(ethToken.increaseAllowance(ethBridge.address, 10, options));
     await w(ambToken.increaseAllowance(ambBridge.address, 10, options));
-
   });
-
-  // it('Amb -> Eth', async () => {
-  // });
 
 
   it('Eth -> Amb', async () => {
+    console.log("ETH");
     // check mint working
     const ethBefore = await ethToken.balanceOf(ethSigner.address);
     const ambBefore = await ambToken.balanceOf(ambSigner.address);
@@ -59,10 +54,10 @@ describe("Integration tests", function () {
     expect(ethBefore).gte(10);
     expect(ambBefore).gte(10);
 
-
     // withdraw
     console.log("Withdraw");
     const fee = await ethBridge.fee();
+
     await w(ethBridge.withdraw(ethToken.address, ambSigner.address, 5, {value: fee, ...options}));
     await sleep(2000); // waiting for 100% new timeframe (timeframe is 1 second)
     await w(ethBridge.withdraw(ethToken.address, ambSigner.address, 1, {value: fee, ...options}));  // must emit event, todo check
@@ -73,12 +68,12 @@ describe("Integration tests", function () {
     await waitConfirmations(minSafetyBlocks, ethSigner.provider);
 
     // wait for transfer submit event (tx from relay)
-    console.log("Waiting for transfer submit event");
-    await new Promise(resolve => ambBridge.once("TransferSubmit", resolve));
+    // console.log("Waiting for transfer submit event");
+    // await new Promise(resolve => ambBridge.once("TransferSubmit", resolve));
 
     // wait for transfer unlock event
-    console.log("Waiting for transfer unlock event");
-    await new Promise(resolve => ambBridge.once("TransferUnlock", resolve));
+    // console.log("Waiting for transfer unlock event");
+    // await new Promise(resolve => ambBridge.once("TransferUnlock", resolve));
 
     // todo check event_id in events
 
@@ -87,8 +82,57 @@ describe("Integration tests", function () {
     const ethAfter = await ethToken.balanceOf(ethSigner.address);
     const ambAfter = await ambToken.balanceOf(ambSigner.address);
 
-    expect(ethAfter).eq(ethBefore - 5);
-    expect(ambAfter).eq(ambBefore + 5);
+    // expect(ethAfter).eq(ethBefore - 5);
+    // expect(ambAfter).eq(ambBefore + 5);
+
+  });
+
+  it('Amb -> Eth', async () => {
+    console.log("AMB");
+    // check mint working
+    const ethBefore = await ethToken.balanceOf(ethSigner.address);
+    const ambBefore = await ambToken.balanceOf(ambSigner.address);
+
+    expect(ethBefore).gte(8);
+    expect(ambBefore).gte(12);
+
+
+    // withdraw
+    console.log("Withdraw");
+    const fee = await ambBridge.fee();
+    const receipt1 = await w(ambBridge.withdraw(ambToken.address, ethSigner.address, 5, {value: fee, gasLimit: 800000}));
+    const events1 = getEvents(receipt1, "Withdraw");
+    const prevEventId = events1[0].args.event_id;
+
+    await sleep(2000); // waiting for 100% new timeframe (timeframe is 1 second)
+
+    const receipt2 = await w(ambBridge.withdraw(ambToken.address, ethSigner.address, 1, {value: fee, gasLimit: 800000}));  // must emit event, todo check
+    const events2 = getEvents(receipt2, "Withdraw");
+    const args = events2[0].args;
+    expect([args.from, args.event_id, args.feeAmount]).to.eql([ambSigner.address, prevEventId.add("1"), fee]);
+
+    // wait for minSafetyBlocks confirmations
+    console.log(`Wait for confirmations`);
+    const minSafetyBlocks = await ethBridge.minSafetyBlocks();
+    await waitConfirmations(minSafetyBlocks, ambSigner.provider);
+
+    // wait for transfer submit event (tx from relay)
+    // console.log("Waiting for transfer submit event");
+    // await new Promise(resolve => ambBridge.once("TransferSubmit", resolve));
+
+    // wait for transfer unlock event
+    // console.log("Waiting for transfer unlock event");
+    // await new Promise(resolve => ambBridge.once("TransferUnlock", resolve));
+
+    // todo check event_id in events
+
+
+    // check user balances
+    const ethAfter = await ethToken.balanceOf(ethSigner.address);
+    const ambAfter = await ambToken.balanceOf(ambSigner.address);
+
+    // expect(ethAfter).eq(ethBefore - 5);
+    // expect(ambAfter).eq(ambBefore + 5);
 
   });
 
@@ -98,7 +142,7 @@ describe("Integration tests", function () {
 
 
 async function waitConfirmations(minSafetyBlocks: number, provider: providers.Provider) {
-  const currentBlock = (await ethSigner.provider.getBlock('latest')).number;
+  const currentBlock = (await provider.getBlock('latest')).number;
   await new Promise<void>((resolve) => {
     provider.on('block', async (block) => {
       if (block - currentBlock > minSafetyBlocks) {
@@ -111,7 +155,11 @@ async function waitConfirmations(minSafetyBlocks: number, provider: providers.Pr
 
 }
 
-
+const getEvents = (receipt: any, eventName: string) => {
+  return receipt.events?.filter((x: any) => {
+    return x.event === eventName;
+  });
+};
 
 
 function sleep(ms: number) {
