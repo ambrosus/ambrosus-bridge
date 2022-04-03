58436dbaaaffeb87f2a8d92cb8dfee089915ddad
svinerus <svinerus@gmail.com>
2022-04-03 17:16:16 +0300

    Refactor deploy script

--- contracts/deploy/for_tests.ts
+++ contracts/deploy/for_tests.ts
@@ -1,112 +1,72 @@
-import {
-    HardhatRuntimeEnvironment
-} from "hardhat/types";
-import {
-    DeployFunction
-} from "hardhat-deploy/types";
+import {HardhatRuntimeEnvironment} from "hardhat/types";
+import {DeployFunction} from "hardhat-deploy/types";
+import {ethers} from "hardhat";
 
-const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
-    if (hre.network.name !== "hardhat") return;
-    const {
-        owner
-    } = await hre.getNamedAccounts();
-    await hre.deployments.deploy("AmbBridgeTest", {
-        from: owner,
-        args: [{
-            sideBridgeAddress: "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-            relayAddress: "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-            tokenThisAddresses: [
-                "0x195c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x395c2707319ad4beca6b5bb4086617fd6f240cfe",
-            ],
-            tokenSideAddresses: [
-                "0x495c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x595c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x695c2707319ad4beca6b5bb4086617fd6f240cfe",
-            ],
-            fee: 1000,
-            feeRecipient: "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-            timeframeSeconds: 14400,
-            lockTime: 1000,
-            minSafetyBlocks: 10,
-        },
-            "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-        ],
-        log: true,
-    });
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (hre.network.name !== "hardhat") return;
+  const {owner} = await hre.getNamedAccounts();
 
-    await hre.deployments.deploy("CommonBridge", {
-        from: owner,
-        args: [{
-            sideBridgeAddress: "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-            relayAddress: "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-            tokenThisAddresses: [
-                "0x195c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x395c2707319ad4beca6b5bb4086617fd6f240cfe",
-            ],
-            tokenSideAddresses: [
-                "0x495c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x595c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x695c2707319ad4beca6b5bb4086617fd6f240cfe",
-            ],
-            fee: 1000,
-            feeRecipient: "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-            timeframeSeconds: 14400,
-            lockTime: 1000,
-            minSafetyBlocks: 10,
-        }, ],
-        log: true,
-    });
+  const commonArgs = {
+    sideBridgeAddress: ethers.constants.AddressZero,
+    relayAddress: ethers.constants.AddressZero,
+    tokenThisAddresses: [
+      "0x0000000000000000000000000000000000000001",
+      "0x0000000000000000000000000000000000000003",
+      "0x0000000000000000000000000000000000000005",
+    ],
+    tokenSideAddresses: [
+      "0x0000000000000000000000000000000000000002",
+      "0x0000000000000000000000000000000000000004",
+      "0x0000000000000000000000000000000000000006",
+    ],
+    fee: 1000,
+    feeRecipient: ethers.constants.AddressZero,
+    timeframeSeconds: 14400,
+    lockTime: 1000,
+    minSafetyBlocks: 10,
+  };
 
-    await hre.deployments.deploy("EthBridgeTest", {
-        contract: "EthBridgeTest",
-        from: owner,
-        args: [{
-            sideBridgeAddress: "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-            relayAddress: "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-            tokenThisAddresses: [
-                "0x195c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x395c2707319ad4beca6b5bb4086617fd6f240cfe",
-            ],
-            tokenSideAddresses: [
-                "0x495c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x595c2707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x695c2707319ad4beca6b5bb4086617fd6f240cfe",
-            ],
-            fee: 1000,
-            feeRecipient: "0x295c2707319ad4beca6b5bb4086617fd6f240cfe",
-            timeframeSeconds: 14400,
-            lockTime: 1000,
-            minSafetyBlocks: 10,
-        },
-            [
-                "0x11112707319ad4beca6b5bb4086617fd6f240cfe",
-                "0x22222707319ad4beca6b5bb4086617fd6f240cfe",
-            ], // initial validators
-            "0x495c2707319ad4beca6b5bb4086617fd6f240cfe", // validatorSetAddress_
-            "0x4b59fc006ebe9733c339f7e0125b0a3eecc3397a75379d3bb226b4baefb08b3a", // lastProcessedBlock
-        ],
-        log: true,
-    });
+  await hre.deployments.deploy("CommonBridge", {
+    from: owner,
+    args: [commonArgs],
+    log: true,
+  });
 
-    await hre.deployments.deploy("MockERC20", {
-        contract: "MockERC20",
-        from: owner,
-        args: [
-            ["0xa74393612236d7927edc3947161e7efc2eca7769"], // bridgeAddresses
-        ],
-        log: true,
-    });
+  await hre.deployments.deploy("AmbBridgeTest", {
+    from: owner,
+    args: [commonArgs, ethers.constants.AddressZero],
+    log: true,
+  });
 
-    await hre.deployments.deploy("wAMB", {
-        contract: "wAMB",
-        from: owner,
-        args: ["wAMB", "wAmb"],
-        log: true,
-    });
+  await hre.deployments.deploy("EthBridgeTest", {
+    contract: "EthBridgeTest",
+    from: owner,
+    args: [commonArgs,
+      [ // initial validators
+        "0x0000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000002",
+      ],
+      ethers.constants.AddressZero, // validatorSetAddress_
+      ethers.constants.HashZero, // lastProcessedBlock
+    ],
+    log: true,
+  });
+
+  await hre.deployments.deploy("MockERC20", {
+    contract: "MockERC20",
+    from: owner,
+    args: [
+      [ethers.constants.AddressZero], // bridgeAddresses
+    ],
+    log: true,
+  });
+
+  await hre.deployments.deploy("wAMB", {
+    contract: "wAMB",
+    from: owner,
+    args: ["wAMB", "wAmb"],
+    log: true,
+  });
 };
 
 export default func;
