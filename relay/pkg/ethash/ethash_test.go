4c1f458ab5c2f34f45f3d215ae221ac701e81a12
svinerus <svinerus@gmail.com>
2022-04-05 02:33:04 +0300

    Add tests

--- relay/pkg/ethash/ethash_test.go
+++ relay/pkg/ethash/ethash_test.go
@@ -1,31 +1,42 @@
 package ethash
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEpoch(t *testing.T) {
-	ethash := New("", 0, 0)
-	_, err := ethash.getCache(1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = ethash.getDag(1)
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestEpoch1(t *testing.T) {
+	ethash := New("./test", 0, 0)
 	edata, err := ethash.GetEpochData(1)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	a := fmt.Sprintf("%x", edata.MerkleNodes[0])
-	if a != "e27b1e1d38a0f0cbfa44991921fd28964253c34a5602235017f6495a6f570718" {
-		t.Fatal(a)
+	assert.Equal(t, edata.MerkleNodes[0].String(), "102440233808978497570443060571864861601403660416248037251589510336707164571416")
+}
+
+func TestEpoch475(t *testing.T) {
+	ethash := New("./test", 0, 0)
+	edata, err := ethash.GetEpochData(475)
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	assert.Equal(t, edata.MerkleNodes[0].String(), "86794333638112600585166912134894146973886718995379343243688093087503049446227")
+}
+
+func TestLookup(t *testing.T) {
+	ethash := New("./test", 0, 0)
+
+	l1, l2, err := ethash.GetBlockLookups(14257704, 16957842275414857198, common.HexToHash("0x4e710542b313c81468f2787dd9d1324663b3370ad25cefc0f8be7754257f9b6a"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, l1[0].String(), "83421943766262542122791167114674843962167463147708128675835754176032138682042")
+	assert.Equal(t, l2[0].String(), "44275787240451683914795652699717121364927282182489534770837402673149472584329")
 
 }
 
