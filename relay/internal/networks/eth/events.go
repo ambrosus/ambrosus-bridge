74144426ba10a30de1ed16b72f346324c6c24e6d
nacknime-official <nacknime.official@gmail.com>
2022-04-08 20:24:24 +0300

    refactor(relay/eth): wrap errors in `getBlocksAndEvents`

--- relay/internal/networks/eth/events.go
+++ relay/internal/networks/eth/events.go
@@ -2,6 +2,7 @@ package eth
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 
 	"github.com/ambrosus/ambrosus-bridge/relay/internal/contracts"
@@ -12,23 +13,26 @@ import (
 // todo name
 func (b *Bridge) getBlocksAndEvents(transferEvent *contracts.TransferEvent) (*contracts.CheckPoWPoWProof, error) {
 	safetyBlocks, err := b.sideBridge.GetMinSafetyBlocksNum()
+	if err != nil {
+		return nil, fmt.Errorf("GetMinSafetyBlocksNum in %v: %w", b.sideBridge.Name(), err)
+	}
 	blocks := make([]contracts.CheckPoWBlockPoW, 0, safetyBlocks+1)
 
 	transfer, err := b.encodeTransferEvent(transferEvent)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("encodeTransferEvent: %w", err)
 	}
 
 	for i := uint64(0); i <= safetyBlocks; i++ {
 		targetBlockNum := big.NewInt(int64(transferEvent.Raw.BlockNumber + i))
 		targetBlock, err := b.Client.BlockByNumber(context.Background(), targetBlockNum)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("BlockByNumber: %w", err)
 		}
 
 		encodedBlock, err := EncodeBlock(targetBlock.Header(), i == 0)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("EncodeBlock: %w", err)
 		}
 		blocks = append(blocks, *encodedBlock)
 	}
