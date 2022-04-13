c06d40805d6036ef53a91f5b00b24c08ca81e35f
svinerus <svinerus@gmail.com>
2022-04-14 01:12:30 +0300

    Set big lockTime and timeframeSeconds only on mainnet

--- contracts/deploy/03_ETH.AmbBridge.ts
+++ contracts/deploy/03_ETH.AmbBridge.ts
@@ -1,12 +1,14 @@
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {DeployFunction} from "hardhat-deploy/types";
 import {ethers} from "hardhat";
-import {configPath, getTokenPairs, readConfig, writeConfig} from "./utils";
+import {configPath, getTokenPairs, networkType, readConfig, writeConfig} from "./utils";
 
 const relayAddress = "0x295c2707319ad4beca6b5bb4086617fd6f240cfe" // todo get from something?
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   if (hre.network.live && !hre.network.tags["amb"]) return;
+  const isMainNet = networkType(hre.network) === 'mainnet'
+
   const path = configPath(hre.network);
   let configFile = readConfig(path);
 
@@ -23,8 +25,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         tokenSideAddresses: Object.values(tokenPairs),
         fee: 1000,  // todo
         feeRecipient: owner,   // todo
-        timeframeSeconds: hre.network.live ? 14400 : 1,
-        lockTime: hre.network.live ? 1000 : 1,
+        timeframeSeconds: isMainNet ? 14400 : 1,
+        lockTime: isMainNet ? 1000 : 1,
         minSafetyBlocks: 10,
       },
       configFile.tokens.wAMB.addresses["amb"],
--- contracts/deploy/04_ETH.EthBridge.ts
+++ contracts/deploy/04_ETH.EthBridge.ts
@@ -2,25 +2,21 @@ import {EthereumProvider, HardhatRuntimeEnvironment} from "hardhat/types";
 import {DeployFunction} from "hardhat-deploy/types";
 import vsAbi from "../abi/ModifiedValidatorSet.json";
 import {ethers} from "ethers";
-import {configPath, getTokenPairs, readConfig, urlFromHHProvider, writeConfig} from "./utils";
+import {configPath, getTokenPairs, networkType, readConfig, urlFromHHProvider, writeConfig} from "./utils";
 
 const vsAddress = "0x0000000000000000000000000000000000000F00" // todo get from something?
 const relayAddress = "0x295c2707319ad4beca6b5bb4086617fd6f240cfe" // todo get from something?
 
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  if (hre.network.live && !hre.network.tags["eth"]) {
-    console.log("Skipping ETH bridge deployment on non-ETH network")
-    return;
-  }
-
-  const {owner} = await hre.getNamedAccounts();
+  if (hre.network.live && !hre.network.tags["eth"]) return;
+  const isMainNet = networkType(hre.network) === 'mainnet'
 
   const ambNet = hre.companionNetworks['amb']
   const {address: sideBridgeAddress} = await ambNet.deployments.get('AmbBridge');
   const [initialValidators, lastProcessedBlock] = await getValidators(ambNet.provider);
 
-
+  const {owner} = await hre.getNamedAccounts();
   const tokenPairs = getTokenPairs("amb", "eth", hre.network)
 
   const deployResult = await hre.deployments.deploy("EthBridge", {
@@ -34,8 +30,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         tokenSideAddresses: Object.values(tokenPairs),
         fee: 10,    // todo
         feeRecipient: owner,   // todo
-        timeframeSeconds: hre.network.live ? 14400 : 1,
-        lockTime: hre.network.live ? 1000 : 1,
+        timeframeSeconds: isMainNet ? 14400 : 1,
+        lockTime: isMainNet ? 1000 : 1,
         minSafetyBlocks: 10,
       },
       initialValidators,
