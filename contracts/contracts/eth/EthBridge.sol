3c745faac22ff0f79fee51e8e32ecc0016ae8ad5
svinerus <svinerus@gmail.com>
2022-04-18 17:40:24 +0300

    Little changes

--- contracts/contracts/eth/EthBridge.sol
+++ contracts/contracts/eth/EthBridge.sol
@@ -2,12 +2,10 @@
 pragma solidity 0.8.6;
 
 import "../common/CommonBridge.sol";
-import "../common/CommonStructs.sol";
 import "../checks/CheckAura.sol";
 
 
 contract EthBridge is CommonBridge, CheckAura {
-    address validatorSetAddress;
 
     function initialize(
         CommonStructs.ConstructorArgs memory args,
@@ -21,7 +19,6 @@ contract EthBridge is CommonBridge, CheckAura {
 
     function submitTransferAura(AuraProof memory auraProof) public onlyRole(RELAY_ROLE) whenNotPaused {
         emit TransferSubmit(auraProof.transfer.eventId);
-
         checkEventId(auraProof.transfer.eventId);
         checkAura_(auraProof, minSafetyBlocks, sideBridgeAddress);
         lockTransfers(auraProof.transfer.transfers, auraProof.transfer.eventId);
