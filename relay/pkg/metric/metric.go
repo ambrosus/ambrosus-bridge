9e760d0ca92e401ce4fa1e0aef4603188a9dcd7b
svinerus <svinerus@gmail.com>
2022-04-20 22:18:43 +0300

    Merge metric files in one `metric.go`

--- relay/pkg/metric/metric.go
+++ relay/pkg/metric/metric.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	RelayBalanceGWeiGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "bridge_relay_balance_gwei",
+		Help: "Balance of a relay in the bridge",
+	}, []string{"bridge_name"})
 )
 
 func ServeEndpoint(ip string, port int) error {
