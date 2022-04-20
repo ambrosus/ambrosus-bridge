75a296d28cc4c2d1dccbd82a3039873074db6a12
nacknime-official <nacknime.official@gmail.com>
2022-04-20 17:09:07 +0300

    chore(relay/eth): add current progress in encoding blocks logs

--- relay/internal/networks/eth/events.go
+++ relay/internal/networks/eth/events.go
@@ -29,7 +29,7 @@ func (b *Bridge) getBlocksAndEvents(transferEvent *contracts.BridgeTransfer) (*c
 			return nil, fmt.Errorf("BlockByNumber: %w", err)
 		}
 
-		b.Logger.Debug().Msgf("Encoding block %d...", targetBlock.NumberU64())
+		b.Logger.Debug().Msgf("Encoding block %d... (%d/%d)", targetBlock.NumberU64(), i, safetyBlocks)
 		encodedBlock, err := b.EncodeBlock(targetBlock.Header(), i == 0)
 		if err != nil {
 			return nil, fmt.Errorf("EncodeBlock: %w", err)
