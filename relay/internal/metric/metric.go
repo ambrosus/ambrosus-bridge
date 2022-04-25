7da8b8c43b4d6c2e8787aedd4baba1d7bdb9a9db
svinerus <svinerus@gmail.com>
2022-04-25 18:35:53 +0300

    Add new metrics

--- relay/internal/metric/metric.go
+++ relay/internal/metric/metric.go
@@ -14,6 +14,17 @@ var (
 		Name: "relay_balance",
 		Help: "Balance of a relay in the bridge (in gwei)",
 	}, []string{"bridge_name"})
+
+	// todo use
+	GasPrice = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "gas_price",
+		Help: "Gas price of a transaction (in gwei)",
+	}, []string{"network_name"})
+
+	UsedGas = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "used_gas",
+		Help: "Used gas of a transaction (in gwei)",
+	}, []string{"network_name"})
 )
 
 func ServeEndpoint(ip string, port int) error {
