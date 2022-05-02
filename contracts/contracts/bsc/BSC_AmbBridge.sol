adc02d7998fdff3ddd725870eab1e075c1c63c79
svinerus <svinerus@gmail.com>
2022-05-02 19:10:28 +0300

    Fix, rename

--- contracts/contracts/bsc/BSC_AmbBridge.sol
+++ contracts/contracts/bsc/BSC_AmbBridge.sol
@@ -5,16 +5,16 @@ import "../common/CommonBridge.sol";
 import "../checks/CheckPoSA.sol";
 
 
-contract AmbBridge is CommonBridge, CheckPoSA {
+contract BSC_AmbBridge is CommonBridge, CheckPoSA {
 
     function initialize(CommonStructs.ConstructorArgs memory args) public initializer {
         __CommonBridge_init(args);
     }
 
-    function submitTransferPoW(PoSAProof memory posaProof) public onlyRole(RELAY_ROLE) whenNotPaused {
+    function submitTransferPoSA(PoSAProof calldata posaProof) public onlyRole(RELAY_ROLE) whenNotPaused {
         emit TransferSubmit(posaProof.transfer.eventId);
         checkEventId(posaProof.transfer.eventId);
-        checkPoW_(posaProof, sideBridgeAddress);
+        CheckPoSA_(posaProof, sideBridgeAddress);
         lockTransfers(posaProof.transfer.transfers, posaProof.transfer.eventId);
     }
 
--- contracts/contracts/bsc/BSC_BscBridge.sol
+++ contracts/contracts/bsc/BSC_BscBridge.sol
@@ -5,19 +5,18 @@ import "../common/CommonBridge.sol";
 import "../checks/CheckAura.sol";
 
 
-contract BscBridge is CommonBridge, CheckAura {
+contract BSC_BscBridge is CommonBridge, CheckAura {
 
     function initialize(
         CommonStructs.ConstructorArgs memory args,
         address[] memory initialValidators,
-        address validatorSetAddress,
-        bytes32 lastProcessedBlock
+        address validatorSetAddress
     ) public initializer {
         __CommonBridge_init(args);
-        __CheckAura_init(initialValidators, validatorSetAddress, lastProcessedBlock);
+        __CheckAura_init(initialValidators, validatorSetAddress);
     }
 
-    function submitTransferAura(AuraProof memory auraProof) public onlyRole(RELAY_ROLE) whenNotPaused {
+    function submitTransferAura(AuraProof calldata auraProof) public onlyRole(RELAY_ROLE) whenNotPaused {
         emit TransferSubmit(auraProof.transfer.eventId);
         checkEventId(auraProof.transfer.eventId);
         checkAura_(auraProof, minSafetyBlocks, sideBridgeAddress);
