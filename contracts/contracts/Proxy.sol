3d8e3e5341ef65b22d64672dfdb4c761a6cf27a6
kiw <anon@anon.com>
2022-05-05 18:40:27 +0300

    add functions for upgrade

--- contracts/contracts/Proxy.sol
+++ contracts/contracts/Proxy.sol
@@ -2,12 +2,17 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/proxy/Proxy.sol";
 import "@openzeppelin/contracts/utils/StorageSlot.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "./MultiSigWallet.sol";
+
 
 // todo rename contract
 // todo + create utils dir for multisig and proxy
 contract proxyTransparent is Proxy, MultiSigWallet {
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+    bytes32 private constant _ROLLBACK_SLOT = 0x4910fdfa16fed3260ed0e7147f7cc6da11a60208b5b9406d12a635614ffd9143;
+
+    event Upgraded(address indexed implementation);
 
     constructor(
         address _logic,
@@ -15,9 +20,40 @@ contract proxyTransparent is Proxy, MultiSigWallet {
         address[] memory owners,
         uint _required
 
-    ) MultiSigWallet(owners, _required) {}
+    ) MultiSigWallet(owners, _required) {
+        _upgradeToAndCall(_logic, _data, false);
+    }
 
-    function _implementation() internal view virtual override returns (address impl) {
+    function upgradeTo(address newImplementation) external {
+        _upgradeToAndCall(newImplementation, bytes(""), false);
+    }
+
+    function upgradeToAndCall(address newImplementation, bytes calldata data) external payable {
+        _upgradeToAndCall(newImplementation, data, true);
+    }
+
+    function _upgradeToAndCall(
+        address newImplementation,
+        bytes memory data,
+        bool forceCall
+    ) internal {
+        _upgradeTo(newImplementation);
+        if (data.length > 0 || forceCall) {
+            Address.functionDelegateCall(newImplementation, data);
+        }
+    }
+
+    function _upgradeTo(address newImplementation) internal {
+        _setImplementation(newImplementation);
+        emit Upgraded(newImplementation);
+    }
+
+    function _setImplementation(address newImplementation) private {
+        require(Address.isContract(newImplementation), "ERC1967: new implementation is not a contract");
+        StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value = newImplementation;
+    }
+
+    function _implementation() internal view override returns (address) {
         return StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value;
     }
 
