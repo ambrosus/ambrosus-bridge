01f82b4e8688d1705626decf36baf7031ffe4b9c
kiwvic <75341499+kiwvic@users.noreply.github.com>
2022-05-09 16:22:46 +0300

    test trigger transfers

--- contracts/test/common.ts
+++ contracts/test/common.ts
@@ -214,7 +214,7 @@ describe("Common tests", () => {
   });
 
 
-  describe("Test Transfer lock/unlock/remove", () => {
+  describe("Test Transfer lock/unlock/remove/trigger", () => {
     beforeEach(async () => {
       const data1 = [[mockERC20.address, user, 10]];
       const data2 = [[mockERC20.address, user, 20], [mockERC20.address, user, 30]];
@@ -289,6 +289,19 @@ describe("Common tests", () => {
 
       await expect(() => commonBridge.unlockTransfers(1))
         .to.changeEtherBalance(userS, 25);
+    });
+
+    it("trigger transfers", async () => {
+      const beforeEventOutputEventId = await commonBridge.getOutputEventId();
+
+      const tx = await commonBridge.triggerTransfers();
+      const receipt = await tx.wait();
+      const events = await getEvents(receipt);
+
+      const afterEventOutputEventId = await commonBridge.getOutputEventId();
+
+      expect(events[0].event).eq("Transfer");
+      expect(beforeEventOutputEventId.add("0x1")).eq(afterEventOutputEventId);
     });
   });
 
