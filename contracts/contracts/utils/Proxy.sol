774968ac22a76fc887f5d71012b709fab3f3ae34
kiw <anon@anon.com>
2022-05-13 12:29:54 +0300

    refactor proxy

--- contracts/contracts/utils/Proxy.sol
+++ contracts/contracts/utils/Proxy.sol
@@ -4,15 +4,15 @@ import "@openzeppelin/contracts/proxy/Proxy.sol";
 import "@openzeppelin/contracts/utils/StorageSlot.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "./MultiSigWallet.sol";
+import "hardhat/console.sol";
 
 
 contract proxyMultiSig is Proxy, MultiSigWallet {
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
     bytes32 private constant _ROLLBACK_SLOT = 0x4910fdfa16fed3260ed0e7147f7cc6da11a60208b5b9406d12a635614ffd9143;
-
     bytes32 private constant ADMIN_STORAGE_LOCATION = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
 
-    bytes4 private constant UPGRADE_TO_AND_CALL__SIGNATURE = bytes4(keccak256("upgradeToAndCall_(address,bytes)"));
+    bytes4 private constant _UPGRADE_TO_AND_CALL_SIGNATURE = bytes4(keccak256("_upgradeToAndCall(address,bytes)"));
 
     event Upgraded(address indexed implementation);
 
@@ -24,7 +24,10 @@ contract proxyMultiSig is Proxy, MultiSigWallet {
         uint _required
 
     ) MultiSigWallet(owners, _required) {
-        _upgradeToAndCall(_logic, _data, false);
+        _upgradeTo(_logic);
+        if (_data.length > 0) {
+            Address.functionDelegateCall(_logic, _data);
+        }
 
         StorageSlot.getAddressSlot(ADMIN_STORAGE_LOCATION).value = msg.sender;  // trick the hardhat-deploy
     }
@@ -38,7 +41,7 @@ contract proxyMultiSig is Proxy, MultiSigWallet {
         submitTransaction(
             address(this),
             msg.value,
-            abi.encodeWithSelector(UPGRADE_TO_AND_CALL__SIGNATURE, newImplementation, bytes(""))
+            abi.encodeWithSelector(_UPGRADE_TO_AND_CALL_SIGNATURE, newImplementation, bytes(""))
         );
     }
 
@@ -46,33 +49,21 @@ contract proxyMultiSig is Proxy, MultiSigWallet {
         submitTransaction(
             address(this),
             msg.value,
-            abi.encodeWithSelector(UPGRADE_TO_AND_CALL__SIGNATURE, newImplementation, data)
+            abi.encodeWithSelector(_UPGRADE_TO_AND_CALL_SIGNATURE, newImplementation, data)
         );
     }
 
-    function upgradeToAndCall_(address newImplementation, bytes calldata data) external onlyWallet payable {
-        _upgradeToAndCall(newImplementation, data, false);
-    }
-
-    function _upgradeToAndCall(
-        address newImplementation,
-        bytes memory data,
-        bool forceCall
-    ) internal {
+    function _upgradeToAndCall(address newImplementation, bytes memory data) external onlyWallet payable {
         _upgradeTo(newImplementation);
-        if (data.length > 0 || forceCall) {
+        if (data.length > 0) {
             Address.functionDelegateCall(newImplementation, data);
         }
     }
 
     function _upgradeTo(address newImplementation) internal {
-        _setImplementation(newImplementation);
-        emit Upgraded(newImplementation);
-    }
-
-    function _setImplementation(address newImplementation) private {
         require(Address.isContract(newImplementation), "ERC1967: new implementation is not a contract");
         StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value = newImplementation;
+        emit Upgraded(newImplementation);
     }
 
     function _implementation() internal view override returns (address) {
