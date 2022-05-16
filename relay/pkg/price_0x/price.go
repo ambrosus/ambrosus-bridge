893adf75e29c5a7cdad51c9917ae6a62bb423edb
nacknime-official <nacknime.official@gmail.com>
2022-05-16 21:08:57 +0300

    refactor(relay/fee): use `uint64` as amount instead of `float64` in `price_0x`

--- relay/pkg/price_0x/price.go
+++ relay/pkg/price_0x/price.go
@@ -4,19 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 )
 
 type response struct {
 	Price float64 `json:"price,string"`
 }
 
-func CoinToUSDT(symbol string, amount float64) (float64, error) {
-	url := fmt.Sprintf(
-		"https://api.0x.org/swap/v1/price?sellToken=%s&buyToken=USDT&sellAmount=%s",
-		symbol,
-		strconv.FormatFloat(amount, 'f', -1, 64),
-	)
+func CoinToUSDT(symbol string, amount uint64) (float64, error) {
+	url := fmt.Sprintf("https://api.0x.org/swap/v1/price?sellToken=%s&buyToken=USDT&sellAmount=%d", symbol, amount)
 	resp, err := http.Get(url)
 	if err != nil {
 		return 0, err
