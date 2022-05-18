10567d4031326e74eb0812192f4b14c40ebaa5a7
nacknime-official <nacknime.official@gmail.com>
2022-05-18 21:58:40 +0300

    fix(relay/fee): convert JSON error to string before writing

--- relay/pkg/helpers/http.go
+++ relay/pkg/helpers/http.go
@@ -9,5 +9,5 @@ func JSONError(w http.ResponseWriter, error []byte, code int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(code)
-	fmt.Fprintln(w, error)
+	fmt.Fprintln(w, string(error))
 }
