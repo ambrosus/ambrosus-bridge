7b76b2b44ccd53ca2f6af37857eb22de4640ad64
svinerus <svinerus@gmail.com>
2022-06-16 17:50:27 +0300

    [UNDO ME] Saves for tests

--- contracts/deploy/03_BSC_AmbBridge.ts
+++ contracts/deploy/03_BSC_AmbBridge.ts
@@ -19,8 +19,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const tokenPairs = configFile.getTokenPairs("amb", "bsc")
 
   const bscNet = hre.companionNetworks['bsc'];
-  const [initialEpoch, initialValidators] = await getBscValidators(bscNet);
+  // const [initialEpoch, initialValidators] = await getBscValidators(bscNet);
   const chainId = await bscNet.getChainId();
+  const initialEpoch = 100500
+  const initialValidators = ["0x049153b8dae0a232ac90d20c78f1a5d1de7b7dc5",
+    "0x1284214b9b9c85549ab3d2b972df0deef66ac2c9",
+    "0x35552c16704d214347f29fa77f77da6d75d7c752",
+    "0x980a75ecd1309ea12fa2ed87a8744fbfc9b863d5",
+    "0xa2959d3f95eae5dc7d70144ce1b73b403b7eb6e0",
+    "0xb71b214cb885500844365e95cd9942c7276e7fd8",
+    "0xf474cf03cceff28abc65c9cbae594f725c80e12d",
+  ]
 
   const deployResult = await hre.deployments.deploy(BRIDGE_NAME, {
     contract: BRIDGE_NAME,
