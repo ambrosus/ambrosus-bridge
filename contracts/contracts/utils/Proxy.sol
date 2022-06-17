dd59053d3e24865feb71f7164ef910b4511f0998
svinerus <svinerus@gmail.com>
2022-06-17 17:06:32 +0300

    Remove unused `_ROLLBACK_SLOT` from `Proxy.sol`

--- contracts/contracts/utils/Proxy.sol
+++ contracts/contracts/utils/Proxy.sol
@@ -10,7 +10,6 @@ import "hardhat/console.sol";
 
 contract ProxyMultiSig is Proxy, MultiSigWallet {
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
-    bytes32 private constant _ROLLBACK_SLOT = 0x4910fdfa16fed3260ed0e7147f7cc6da11a60208b5b9406d12a635614ffd9143;
     bytes32 private constant ADMIN_STORAGE_LOCATION = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
 
     bytes4 private constant _UPGRADE_TO_AND_CALL_SIGNATURE = bytes4(keccak256("_upgradeToAndCall(address,bytes)"));
