e561d9ee721c476ef98f2c19dc5c85e6480d39b6
nacknime-official <nacknime.official@gmail.com>
2022-06-17 12:03:06 +0300

    feat(relay/fees): add Logging Middleware

--- relay/internal/fee_api/handlers.go
+++ relay/internal/fee_api/handlers.go
@@ -44,6 +44,7 @@ func (p *FeeAPI) feesHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(result)
 }
 
