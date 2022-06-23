8cb756407f55620d2baf567a0b573dbba7c3165f
nacknime-official <nacknime.official@gmail.com>
2022-06-23 15:03:26 +0300

    feat(contracts/deploy): add script for deploy WETH if it's now mainnet

---
+++ contracts/deploy/00_WETH.ts
@@ -0,0 +1,37 @@
+import {HardhatRuntimeEnvironment} from "hardhat/types";
+import {DeployFunction} from "hardhat-deploy/types";
+import {parseNet, readConfig_} from "./utils/utils";
+import {isAddress} from "ethers/lib/utils";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (hre.network.live && !hre.network.tags["eth"]) return;
+  const isMainNet = parseNet(hre.network).stage === 'main'
+  if (isMainNet) {
+    console.log("No need to deploy WETH on mainnet");
+    return;
+  }
+
+  const {owner} = await hre.getNamedAccounts();
+
+  let configFile = readConfig_(hre.network);
+
+  const weth = configFile.tokens.WETH;
+  if (isAddress(weth.addresses.eth)) {
+    console.log("wETH already deployed");
+    return;
+  }
+
+  const deployResult = await hre.deployments.deploy(weth.symbol, {
+    contract: "sAMB",
+    from: owner,
+    args: [weth.name, weth.symbol],
+    log: true,
+  });
+
+  weth.addresses.eth = deployResult.address;
+  configFile.save();
+
+};
+
+export default func;
+func.tags = ["wETH", "tokens"];
