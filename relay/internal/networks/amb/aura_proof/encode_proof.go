4774007f0e22c937cd9998e9cdd0d9b1bc529bff
svinerus <svinerus@gmail.com>
2022-07-06 04:33:03 +0300

    Refactor amb `encode_proof` pkg

--- relay/internal/networks/amb/aura_proof/encode_proof.go
+++ relay/internal/networks/amb/aura_proof/encode_proof.go
@@ -12,16 +12,8 @@ import (
 	"github.com/ambrosus/ambrosus-bridge/relay/pkg/ethclients/parity"
 	"github.com/ambrosus/ambrosus-bridge/relay/pkg/helpers"
 	"github.com/rs/zerolog"
-
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
+	"golang.org/x/exp/constraints"
 )
-
-type blockExt struct {
-	block             *c.CheckAuraBlockAura
-	finalizedVsEvents []c.CheckAuraValidatorSetChange
-	lastEvent         *c.VsInitiateChange
-}
 
 type AuraEncoder struct {
 	bridge       networks.Bridge
@@ -31,6 +23,9 @@ type AuraEncoder struct {
 	parityClient *parity.Client
 
 	logger *zerolog.Logger
+
+	// cache for fetching blocks. clear (almost) every `EncodeAuraProof` call
+	fetchBlockCache func(arg uint64) (*parity.Header, error)
 }
 
 func NewAuraEncoder(bridge *amb.Bridge, sideBridge networks.BridgeReceiveAura, vSContract *c.Vs, parityClient *parity.Client) *AuraEncoder {
@@ -44,96 +39,81 @@ func NewAuraEncoder(bridge *amb.Bridge, sideBridge networks.BridgeReceiveAura, v
 }
 
 func (b *AuraEncoder) EncodeAuraProof(transferEvent *c.BridgeTransfer, safetyBlocks uint64) (*c.CheckAuraAuraProof, error) {
-	// populated by functions below
-	blocksMap := make(map[uint64]*blockExt)
+	// todo arg for split (reduced size proof)
 
-	// encode transferProof and save event block to blocksMap
-	transfer, err := b.encodeTransferEvent(blocksMap, transferEvent, safetyBlocks)
+	// new cache for every call
+	// todo don't clear cache for reduced size proof
+	b.fetchBlockCache = NewCache(b.fetchBlock)
+
+	var blocksToSave []uint64
+
+	lastBlock := transferEvent.Raw.BlockNumber + safetyBlocks
+
+	// todo don't encode and save blocks for reduced size proof
+	transferProof, err := b.encodeTransferProof(transferEvent)
 	if err != nil {
-		return nil, fmt.Errorf("encodeTransferEvent: %w", err)
+		return nil, fmt.Errorf("encodeTransferProof: %w", err)
 	}
 
-	// encode vsChangeProofs and save event blocks to blocksMap
-	vsChangeEvents, err := b.fetchVSChangeEvents(transferEvent, safetyBlocks)
+	// save blocks for transfer
+	blocksToSave = append(blocksToSave, Range(transferEvent.Raw.BlockNumber, lastBlock+1)...)
+
+	// lastBlock can be decreased if proof is too big
+	// todo better name
+	vsChangesExt, err := b.getVsChanges(lastBlock)
 	if err != nil {
-		return nil, fmt.Errorf("fetchVSChangeEvents: %w", err)
-	}
-	err = b.encodeVSChangeEvents(blocksMap, vsChangeEvents)
-	if err != nil {
-		return nil, fmt.Errorf("encodeVSChangeEvents: %w", err)
+		return nil, fmt.Errorf("getVsChanges: %w", err)
 	}
 
-	// sort blocks in blocksMap and use resulting indexes
-	indexToBlockNum := helpers.SortedKeys(blocksMap)
-	var blocks []c.CheckAuraBlockAura
+	// save blocks for vs change events
+	safetyBlocksValidators, err := b.auraReceiver.GetMinSafetyBlocksValidators()
+	if err != nil {
+		return nil, fmt.Errorf("GetMinSafetyBlocksValidators: %w", err)
+	}
+	for _, vsChange := range vsChangesExt {
+		blocksToSave = append(blocksToSave, Range(vsChange.eventBlock, vsChange.eventBlock+safetyBlocksValidators+1)...)
+		// gap
+		blocksToSave = append(blocksToSave, vsChange.finalizedBlock)
+	}
+
+	// fetch and encode blocksToSave
+	blocks, blockNumToIndex, err := b.saveEncodedBlocks(blocksToSave)
+	if err != nil {
+		return nil, fmt.Errorf("saveEncodedBlocks: %w", err)
+	}
+
+	// for each finalized event save it into `vsChanges` and set it `index+1` to block `FinalizedVs` field
 	var vsChanges []c.CheckAuraValidatorSetProof
-	var transferEventIndex uint64
-	blocksToIndex := make(map[uint64]uint64) // need for `EventBlock` in vsChange
+	for _, blockWithEvent := range helpers.SortedKeys(vsChangesExt) {
+		vsChangeEvent := vsChangesExt[blockWithEvent]
+		finalizedBlockIndex := blockNumToIndex[vsChangeEvent.finalizedBlock]
 
-	for i, blockNum := range indexToBlockNum {
-		blocksToIndex[blockNum] = uint64(i)
-
-		// fill up 'blocks'
-		blocks = append(blocks, *blocksMap[blockNum].block)
-
-		// fill up 'vsChanges'
-		if blocksMap[blockNum].lastEvent != nil {
-			proof, err := cb.GetProof(b.bridge.GetClient(), blocksMap[blockNum].lastEvent)
-			if err != nil {
-				return nil, fmt.Errorf("GetProof: %w", err)
-			}
-			vsChanges = append(vsChanges, c.CheckAuraValidatorSetProof{
-				ReceiptProof: proof,
-				Changes:      blocksMap[blockNum].finalizedVsEvents,
-				EventBlock:   big.NewInt(int64(blocksToIndex[blocksMap[blockNum].lastEvent.Raw.BlockNumber])),
-			})
-
-			// in this block contract should finalize all events in vsChanges array up to `FinalizedVs` index
-			blocks[i].FinalizedVs = uint64(len(vsChanges))
+		proof, err := cb.GetProof(b.bridge.GetClient(), vsChangeEvent.lastEvent)
+		if err != nil {
+			return nil, fmt.Errorf("GetProof: %w", err)
 		}
+		vsChanges = append(vsChanges, c.CheckAuraValidatorSetProof{
+			ReceiptProof: proof,
+			Changes:      vsChangeEvent.changes,
+			EventBlock:   big.NewInt(int64(blockNumToIndex[vsChangeEvent.eventBlock])), // todo uint64
+		})
 
-		// set transferEventIndex to index in blocks array
-		if blockNum == transferEvent.Raw.BlockNumber {
-			transferEventIndex = uint64(i)
-		}
+		// in this Block contract should finalize all events in vsChanges array up to `FinalizedVs` index
+		blocks[finalizedBlockIndex].FinalizedVs = uint64(len(vsChanges))
 	}
+
 	return &c.CheckAuraAuraProof{
 		Blocks:             blocks,
-		Transfer:           *transfer,
+		Transfer:           *transferProof,
 		VsChanges:          vsChanges,
-		TransferEventBlock: transferEventIndex,
+		TransferEventBlock: uint64(blockNumToIndex[transferEvent.Raw.BlockNumber]),
 	}, nil
+
 }
 
-// if proof too long we need to split it into smaller parts
-func (b *AuraEncoder) splitVsChanges(proof *c.CheckAuraAuraProof) *c.CheckAuraAuraProof {
-	b.logger.Warn().Int("blocks", len(proof.Blocks)).Msgf("Aura proof too long")
-	blocks := proof.Blocks[:len(proof.Blocks)/2] // drop half of blocks
-	// todo maybe keep ~3000 blocks instead of half
-
-	// last block should be vsFinalize
-	var vsChanges []c.CheckAuraValidatorSetProof
-	for i := uint64(len(blocks)); i > 0; i-- {
-		if blocks[i].FinalizedVs == 0 {
-			blocks = blocks[:i+1]
-			vsChanges = proof.VsChanges[:blocks[i].FinalizedVs+1]
-		}
-	}
-
-	return &c.CheckAuraAuraProof{
-		Blocks:    blocks,
-		VsChanges: vsChanges,
-	}
-}
-
-func (b *AuraEncoder) encodeTransferEvent(blocks map[uint64]*blockExt, event *c.BridgeTransfer, safetyBlocks uint64) (*c.CommonStructsTransferProof, error) {
+func (b *AuraEncoder) encodeTransferProof(event *c.BridgeTransfer) (*c.CommonStructsTransferProof, error) {
 	proof, err := cb.GetProof(b.bridge.GetClient(), event)
 	if err != nil {
-		return nil, err
-	}
-
-	// save `safetyBlocks` blocks after event block
-	if err = b.saveBlocksRange(blocks, event.Raw.BlockNumber, event.Raw.BlockNumber+safetyBlocks); err != nil {
 		return nil, err
 	}
 
@@ -144,142 +124,44 @@ func (b *AuraEncoder) encodeTransferEvent(blocks map[uint64]*blockExt, event *c.
 	}, nil
 }
 
-func (b *AuraEncoder) encodeVSChangeEvents(blocks map[uint64]*blockExt, events []*c.VsInitiateChange) error {
-	prevSet, err := b.auraReceiver.GetValidatorSet()
-	if err != nil {
-		return fmt.Errorf("GetValidatorSet: %w", err)
-	}
+func (b *AuraEncoder) saveEncodedBlocks(blockNums []uint64) (blocks []c.CheckAuraBlockAura, blockNumToIndex map[uint64]int, err error) {
+	blocks = make([]c.CheckAuraBlockAura, len(blockNums))
 
-	minSafetyBlocksValidators, err := b.auraReceiver.GetMinSafetyBlocksValidators()
-	if err != nil {
-		return fmt.Errorf("GetMinSafetyBlocksValidators: %w", err)
-	}
-
-	var lastBlock uint64
-	var txsBeforeFinalize uint64
-	for _, event := range events {
-		address, index, err := deltaVS(prevSet, event.NewSet)
+	for i, bn := range blockNums {
+		block, err := b.fetchBlockCache(bn)
 		if err != nil {
-			return fmt.Errorf("deltaVS: %w", err)
+			return nil, nil, fmt.Errorf("fetchBlockCache: %w", err)
 		}
-		vsChange := c.CheckAuraValidatorSetChange{DeltaAddress: address, DeltaIndex: index}
-
-		if lastBlock != event.Raw.BlockNumber {
-			txsBeforeFinalize = uint64(len(prevSet))/2 + 1
-			lastBlock = event.Raw.BlockNumber
-		}
-		finalizedBlockNum := event.Raw.BlockNumber + txsBeforeFinalize
-		safetyEndBlockNum := event.Raw.BlockNumber + minSafetyBlocksValidators
-
-		// save blocks up to finalized block
-		if err = b.saveBlocksRange(blocks, event.Raw.BlockNumber, safetyEndBlockNum); err != nil {
-			return err
+		encodedBlock, err := EncodeBlock(block)
+		if err != nil {
+			return nil, nil, fmt.Errorf("EncodeBlock: %w", err)
 		}
 
-		// block in which VS will be finalized
-		if err := b.saveBlock(blocks, finalizedBlockNum); err != nil {
-			return err
-		}
-		blockWhenFinalize := blocks[finalizedBlockNum]
-		blockWhenFinalize.finalizedVsEvents = append(blockWhenFinalize.finalizedVsEvents, vsChange)
-		blockWhenFinalize.lastEvent = event
-
-		prevSet = event.NewSet
+		blocks[i] = *encodedBlock
+		blockNumToIndex[bn] = i
 	}
-	return nil
+
+	return blocks, blockNumToIndex, nil
 }
 
-func (b *AuraEncoder) fetchVSChangeEvents(event *c.BridgeTransfer, safetyBlocks uint64) ([]*c.VsInitiateChange, error) {
-	start, err := b.getLastProcessedBlockNum()
-	if err != nil {
-		return nil, fmt.Errorf("getLastProcessedBlockNum: %w", err)
-	}
-	end := event.Raw.BlockNumber + safetyBlocks - 1 // no need to change vs for last block (it will be changed in next event processing)
-
-	opts := &bind.FilterOpts{
-		Start:   start.Uint64(),
-		End:     &end,
-		Context: context.Background(),
-	}
-
-	logs, err := b.vsContract.FilterInitiateChange(opts, nil)
-	if err != nil {
-		return nil, fmt.Errorf("filter initiate changes: %w", err)
-	}
-
-	var res []*c.VsInitiateChange
-	for logs.Next() {
-		res = append(res, logs.Event)
-	}
-
-	return res, nil
+func (b *AuraEncoder) fetchBlock(blockNum uint64) (*parity.Header, error) {
+	return b.parityClient.ParityHeaderByNumber(context.Background(), big.NewInt(int64(blockNum)))
 }
 
-func (b *AuraEncoder) getLastProcessedBlockNum() (*big.Int, error) {
-	blockHash, err := b.auraReceiver.GetLastProcessedBlockHash()
-	if err != nil {
-		return nil, fmt.Errorf("GetLastProcessedBlockHash: %w", err)
-	}
-
-	block, err := b.bridge.GetClient().BlockByHash(context.Background(), *blockHash)
-	if err != nil {
-		return nil, fmt.Errorf("get block by hash: %w", err)
-	}
-
-	return block.Number(), nil
-}
-
-func deltaVS(prev, curr []common.Address) (common.Address, int64, error) {
-	d := len(curr) - len(prev)
-	if d != 1 && d != -1 {
-		return common.Address{}, 0, fmt.Errorf("delta has more (or less) than 1 change")
-	}
-
-	for i, prevEl := range prev {
-		if i >= len(curr) { // deleted at the end
-			return prev[i], int64(-i - 1), nil
+func NewCache[K comparable, V any](getter func(K) (V, error)) func(arg K) (V, error) {
+	cache := map[K]V{}
+	return func(arg K) (V, error) {
+		if v, ok := cache[arg]; ok {
+			return v, nil
 		}
-
-		if curr[i] != prevEl {
-			if d == 1 { // added
-				return curr[i], int64(i), nil
-			} else { // deleted
-				return prev[i], int64(-i - 1), nil
-			}
-		}
+		return getter(arg)
 	}
-
-	// add at the end
-	i := len(curr) - 1
-	return curr[i], int64(i), nil
-
-	// return common.Address{}, 0, fmt.Errorf("this error shouln't exist")
 }
 
-// save blocks from `from` to `to` INCLUSIVE
-func (b *AuraEncoder) saveBlocksRange(blocksMap map[uint64]*blockExt, from, to uint64) error {
-	for i := from; i <= to; i++ {
-		if err := b.saveBlock(blocksMap, i); err != nil {
-			return err
-		}
+func Range[T constraints.Integer](start, end T) []T {
+	res := make([]T, end-start)
+	for i := start; i < end; i++ {
+		res = append(res, i)
 	}
-	return nil
-}
-
-func (b *AuraEncoder) saveBlock(blocksMap map[uint64]*blockExt, blockNumber uint64) error {
-	if _, ok := blocksMap[blockNumber]; ok {
-		return nil
-	}
-
-	block, err := b.parityClient.ParityHeaderByNumber(context.Background(), big.NewInt(int64(blockNumber)))
-	if err != nil {
-		return fmt.Errorf("HeaderByNumber: %w", err)
-	}
-	encodedBlock, err := EncodeBlock(block)
-	if err != nil {
-		return fmt.Errorf("encode: %w", err)
-	}
-
-	blocksMap[blockNumber] = &blockExt{block: encodedBlock}
-	return nil
+	return res
 }
