35997bb04e046400df54f200004fb65104819959
svinerus <svinerus@gmail.com>
2022-07-12 05:57:50 +0300

    Move common code from `BSC_BscBridge.sol` and `ETH_EthBridge.sol` to `_AuraReceiver.sol`

--- contracts/contracts/networks/BSC_BscBridge.sol
+++ contracts/contracts/networks/BSC_BscBridge.sol
@@ -1,36 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-import "../common/CommonBridge.sol";
-import "../checks/CheckAura.sol";
+import "./_AuraReceiver.sol";
 
 
-contract BSC_BscBridge is CommonBridge, CheckAura {
+contract BSC_BscBridge is _AuraReceiver {
 
-    function initialize(
-        CommonStructs.ConstructorArgs calldata args,
-        address[] calldata initialValidators,
-        address validatorSetAddress,
-        bytes32 lastProcessedBlock,
-        uint minSafetyBlocksValidators
-    ) public initializer {
-        __CommonBridge_init(args);
-        __CheckAura_init(initialValidators, validatorSetAddress, lastProcessedBlock, minSafetyBlocksValidators);
-    }
-
-
-    function changeMinSafetyBlocksValidators(uint minSafetyBlocksValidators_) public onlyRole(ADMIN_ROLE) {
-        minSafetyBlocksValidators = minSafetyBlocksValidators_;
-    }
-
-    function submitTransferAura(AuraProof calldata auraProof) public onlyRole(RELAY_ROLE) whenNotPaused {
-        emit TransferSubmit(auraProof.transfer.eventId);
-        checkEventId(auraProof.transfer.eventId);
-        checkAura_(auraProof, minSafetyBlocks, sideBridgeAddress);
-        lockTransfers(auraProof.transfer.transfers, auraProof.transfer.eventId);
-    }
-
-    function submitValidatorSetChangesAura(AuraProof calldata auraProof) public onlyRole(RELAY_ROLE) whenNotPaused {
-        checkAura_(auraProof, minSafetyBlocks, sideBridgeAddress);
-    }
 }
