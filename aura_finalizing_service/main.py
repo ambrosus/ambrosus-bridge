590a54befed4bbf97ac3c1443eae5c8a79614007
svinerus <svinerus@gmail.com>
2022-07-15 00:13:29 +0300

    Rework `aura_finalizing_service` to filter logs by python, not by loki

--- aura_finalizing_service/main.py
+++ aura_finalizing_service/main.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 from collections import namedtuple
+from dataclasses import dataclass
 from pathlib import Path
 from pprint import pprint
 
@@ -12,9 +13,16 @@ LAST_LOG_FILE = Path.cwd() / "files" / "last_log.txt"
 
 LOKI_URL = os.getenv("LOKI_URL")  # ex: 'loki:QWE123qwe123qwe123qwe@loki.ambrosus-ops.io'
 LOKI_PARAMS = os.getenv("LOKI_PARAMS")  # ex: '{host="dev-parity0", container_name="parity"}'
+LOKI_LIMIT = 5000
 
 LOG_RE = re.compile(r'epoch \((\d+), .* #(\d+)')
-Log = namedtuple("Log", "timestamp finalize_at signaled_at")
+
+
+@dataclass
+class Log:
+    timestamp: int
+    finalize_at: int
+    signaled_at: int
 
 
 def main():
@@ -28,34 +36,38 @@ def main():
             print("Exception while fetching logs", e)
             time.sleep(60)
             continue
+        print(f"fetched {len(logs)} logs")
 
-        logs = list(filter(
+        finalize_logs = filter(None, map(parse_loki_log, logs))  # filter out not finalize logs
+        finalize_logs = list(filter(
             lambda log, last_log_=last_log: log.finalize_at < last_log_.finalize_at,
-            logs))  # drop already saved logs
+            finalize_logs))  # filter out already saved logs
 
-        if not logs:  # if no new logs => sleep 1 min
-            print("no new logs, sleep 60 sec")
-            # last_log.timestamp = time.time_ns()  # may be inaccurate
-            time.sleep(60)
-        else:
-            last_log = logs[-1]
+        last_log.timestamp = int(logs[-1][0])  # set last_log timestamp to last log timestamp 🤯
+
+        if finalize_logs:
+            # set last_log last block to last finalize log block to avoid doubles
+            last_log.finalize_at = finalize_logs[-1].finalize_at
             print(f"find {len(logs)} new logs, latest is {last_log}")
             save_new_logs(logs)
+
+        if len(logs) < LOKI_LIMIT:
+            print(f"Fetched less logs than limit ({len(logs)} / {LOKI_LIMIT}); Sleep 60 sec")
+            time.sleep(60)
 
 
 def fetch_logs(from_timestamp: int) -> [Log]:
     print(f"fetching logs {from_timestamp=}")
     r = requests.get(f"https://{LOKI_URL}/loki/api/v1/query_range", params={
-        "query": f'{LOKI_PARAMS} |= "extracting epoch validator set for epoch"',
+        "query": LOKI_PARAMS,
         "start": from_timestamp,
-        "limit": "5000",
+        "limit": LOKI_LIMIT,
         "direction": "forward",
     })
-    pprint(r.json())
     data_result = r.json()["data"]["result"]
     if not data_result:
         return []
-    return [parse_loki_log(log) for log in data_result[0]["values"]]
+    return data_result[0]["values"]
 
 
 def parse_loki_log(log: [str, str]) -> Log:
@@ -66,8 +78,12 @@ def parse_loki_log(log: [str, str]) -> Log:
     '2022-07-14 11:36:10 UTC Verifier #0 TRACE engine  extracting epoch validator set'
     'for epoch (216781, 0x222c…b6b0) signalled at #216776']
     """
+    regex_match = LOG_RE.findall(log[1])
+    if not regex_match:
+        return None
+
     timestamp = log[0]
-    finalize_at, signaled_at = LOG_RE.findall(log[1])[0]
+    finalize_at, signaled_at = regex_match[0]
     return Log(int(timestamp), int(finalize_at), int(signaled_at))
 
 
