db8b176497d8c383fd824645e571d934fcb8aad9
nacknime-official <nacknime.official@gmail.com>
2022-07-15 08:26:23 +0300

    refactor(relay): make max tx size configurable; move `IsProofTooBig` func into `bindings`

--- relay/internal/config/structs_services.go
+++ relay/internal/config/structs_services.go
@@ -5,16 +5,20 @@ type (
 		enable `mapstructure:",squash"`
 		Aura   *SubmitterAura `mapstructure:"aura"`
 		Pow    *SubmitterPoW  `mapstructure:"pow"`
-		//Posa    *SubmitterPoSA `mapstructure:"posa"`  PoSA doesn't need any cfg
+		Posa   *SubmitterPoSA `mapstructure:"posa"`
 	}
 
 	SubmitterAura struct {
-		VSContractAddr string `mapstructure:"vsContractAddr"`
+		VSContractAddr            string `mapstructure:"vsContractAddr"`
+		ReceiverBridgeMaxTxSizeKB uint64 `mapstructure:"receiverBridgeMaxTxSizeKB"`
 	}
 	SubmitterPoW struct {
 		EthashDir            string `mapstructure:"ethashDir"`
 		EthashKeepPrevEpochs uint64 `mapstructure:"ethashKeepPrevEpochs"`
 		EthashGenNextEpochs  uint64 `mapstructure:"ethashGenNextEpochs"`
+	}
+	SubmitterPoSA struct {
+		ReceiverBridgeMaxTxSizeKB uint64 `mapstructure:"receiverBridgeMaxTxSizeKB"`
 	}
 )
 
