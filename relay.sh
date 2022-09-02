c20a65aafcf76fb4e829e89166a3e2d5f4261f19
november-pain <max.chikita@gmail.com>
2022-09-02 21:57:24 +0200

    add key length check

--- relay.sh
+++ relay.sh
@@ -24,6 +24,10 @@ echo "Please enter your private key"
 
 read -sp 'Ambrosus private key: ' amb_private_key
 
+if [ ${#amb_private_key} -ne 64 ];
+then read -sp 'Key length should be 64 characters, type again: ' amb_private_key ; exit
+fi
+
 set +e
 docker rm -f eth-relay
 set -e
