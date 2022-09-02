85658f714bc4ab41dc9f894197f778f77cca3e46
november-pain <max.chikita@gmail.com>
2022-09-02 21:41:13 +0200

    script for deploying relay

---
+++ relay.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+apt-get remove -y docker docker-engine docker.io containerd runc && \
+apt-get update && \
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+echo "Please enter your private key"
+
+read -sp 'Ambrosus private key: ' amb_private_key
+
+set +e
+docker rm -f eth-relay
+set -e
+
+echo "Starting relay..."
+docker run -d \
+--name eth-relay \
+--restart unless-stopped \
+-e STAGE=prod \
+-e NETWORK=eth-untrustless \
+-e NETWORKS_AMB_PRIVATEKEY=$amb_private_key \
+ghcr.io/ambrosus/ambrosus-bridge >> /dev/null
+
+sleep 10
+docker logs eth-relay
