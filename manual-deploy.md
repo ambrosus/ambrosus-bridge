81319e93d71429d731cfa4fd8f6325257ec268d9
SvineruS <37367870+SvineruS@users.noreply.github.com>
2022-09-06 16:48:38 +0300

    Manual deploy (#313)

---
+++ manual-deploy.md
@@ -0,0 +1,28 @@
+## This is manual to deploy temporary relay instance for AMB-ETH bridge.
+
+### Installation
+
+##### Creating a Droplet
+
+Typical DigitalOcean node (2 GB / 2 CPUs, 60 GB SSD disk, 3 TB transfer) should be good for running a relay
+
+There is detailed step by step information how to setup droplet on digitalocean https://www.digitalocean.com/docs/droplets/how-to/create/
+
+Our brief instructions:
+
+Create an account and log in. Press 'Droplets' and then 'Create Droplet'. Use the OS Ubuntu and then choose what machine preferences and which data center suits you. Then either create a SSH key which you will use to access the instance or if you do not choose one you will get a password to your email. Write a hostname that suits you and launch the instance.
+
+### Obtaining private key
+
+You can export the private key from your metamask wallet using this guide:
+https://metamask.zendesk.com/hc/en-us/articles/360015289632-How-to-export-an-account-s-private-key
+
+### Run
+
+Command to run eth-relay:
+
+<pre>
+curl -s https://raw.githubusercontent.com/ambrosus/ambrosus-bridge/manual-deploy/relay.sh > relay.sh && sudo chmod +x relay.sh && sudo ./relay.sh
+</pre>
+
+If any pop-ups occur, select "ok".  
---
+++ relay.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -e
+
+apt-get remove -y docker docker-engine docker.io containerd runc && \
+apt-get update && \
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+read -p "Please choose your network(dev/test/prod): " stage;
+while true;
+do
+    echo -e "\n"
+    if [[ "$stage" =~ ^(dev|test|prod)$ ]];
+        then break
+        else read -p "You entered incorect network. Please choose your network(dev/test/prod): " stage
+    fi
+done
+
+echo "Please enter your private key"
+
+read -sp 'Ambrosus private key: ' amb_private_key
+while true;
+do
+    echo -e "\n"
+    if [ ${#amb_private_key} -ne 64 ];
+        then read -sp 'Key length should be 64 characters, type again: ' amb_private_key;
+        else break
+    fi
+done
+
+set +e
+docker rm -f eth-relay
+set -e
+
+echo "Starting relay..."
+docker run -d \
+--name eth-relay \
+--restart unless-stopped \
+-e STAGE=$stage \
+-e NETWORK=eth-untrustless \
+-e NETWORKS_AMB_PRIVATEKEY=$amb_private_key \
+ghcr.io/ambrosus/ambrosus-bridge:dev >> /dev/null
+
+sleep 10
+docker logs eth-relay
