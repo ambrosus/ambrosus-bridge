2e9c148053e57980207224d6e1c826748164d1fc
svinerus <svinerus@gmail.com>
2022-09-14 04:36:35 +0300

    Put back `setBridgeAddressesRole` method call for backward compatibility

--- contracts/deploy/05_ERC20Tokens_Add_Bridges.ts
+++ contracts/deploy/05_ERC20Tokens_Add_Bridges.ts
@@ -7,7 +7,10 @@ import {DeployFunction} from "hardhat-deploy/types";
 import {getBridgesDecimals, parseNet, readConfig_} from "./utils/utils";
 import {ethers} from "ethers";
 import {isAddress} from "ethers/lib/utils";
-import {isTokenNotBridgeERC20, Token} from "./utils/config";
+import {Config, isTokenNotBridgeERC20, Token} from "./utils/config";
+
+
+const BRIDGE_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("BRIDGE_ROLE"));
 
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
@@ -18,6 +21,22 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   // add bridge address to tokens
   console.log("add bridge address to tokens")
 
+
+  // try to use old BridgeERC20 method for backward compatibility with legacy contracts
+  const addLegacy = async (token: Token) => {
+    const notSetBridges = (await Promise.all(
+      bridgesInNet(netName, configFile)
+        .map(async (br) => {
+          const hasRole = await hre.deployments.read(token.symbol, {from: owner}, "hasRole", BRIDGE_ROLE, br)
+          return hasRole ? null : br
+        })))
+      .filter(v => v != null)
+
+
+    if (notSetBridges.length > 0)
+      await hre.deployments.execute(token.symbol, {from: owner, log: true},
+        "setBridgeAddressesRole", notSetBridges)
+  }
 
   // can have more than 1 bridge addresses; can convert decimals between networks
   const addAmb = async (token: Token) => {
@@ -53,11 +72,29 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     if (!isAddress(token.addresses[netName])) continue;  // not deployed
     if (isTokenNotBridgeERC20(token, netName)) continue;  // it's not bridgeErc20, no need to set role
 
-    if (hre.network.tags["amb"]) await addAmb(token);
-    else await addNonAmb(token);
+    try {
+      await addLegacy(token) // try to use old method first
+    } catch (e) {
+      if (netName == "amb") {
+        await addAmb(token)
+      } else {
+        await addNonAmb(token)
+      }
+    }
+
   }
 
 };
+
+// get all deployed bridges in `net` network;
+// for amb it's array of amb addresses for each network pair (such "amb-eth" or "amb-bsc")
+// for other networks is array of one address
+function bridgesInNet(net: string, configFile: Config): string[] {
+  const bridges = (net == "amb") ?
+    Object.values(configFile.bridges).map(i => i.amb) :
+    [configFile.bridges[net].side];
+  return bridges.filter(i => !!i);  // filter out empty strings
+}
 
 
 export default func;
