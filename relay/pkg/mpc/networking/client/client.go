5582c938be71d724b322c9a01ce2f36b83920a1f
svinerus <svinerus@gmail.com>
2022-10-04 02:59:29 +0300

    Another merge fix

--- relay/pkg/mpc/networking/client/client.go
+++ relay/pkg/mpc/networking/client/client.go
@@ -23,15 +23,6 @@ type Client struct {
 	serverURL string
 }
 
-func NewClient(tss *tss_wrap.Mpc, serverURL string, logger *zerolog.Logger) *Client {
-	s := &Client{
-		tss:       tss,
-		serverURL: serverURL,
-		Logger:    logger,
-	}
-	return s
-}
-
 type operation struct {
 	started bool
 	// will be sent to tss
