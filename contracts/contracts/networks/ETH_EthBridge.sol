4b4c83c846b261802570b5719f134742e96c20e4
svinerus <svinerus@gmail.com>
2022-10-13 19:40:43 +0300

    Update `upgrade` function and deploy script in `ETH_EthBridge`

--- contracts/contracts/networks/ETH_EthBridge.sol
+++ contracts/contracts/networks/ETH_EthBridge.sol
@@ -15,11 +15,19 @@ contract ETH_EthBridge is CommonBridge, CheckUntrustless2 {
 
     function upgrade(
         address[] calldata _watchdogs,
-        address _fee_provider
+        address _fee_provider,
+        address oldDefaultAdmin
     ) public {
         require(msg.sender == address(this), "This method require multisig");
+
+        // new roles for untrustless mpc
         _setupRoles(WATCHDOG_ROLE, _watchdogs);
         _setupRole(FEE_PROVIDER_ROLE, _fee_provider);
+
+        // add DEFAULT_ADMIN_ROLE to multisig
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        // revoke DEFAULT_ADMIN_ROLE from deployer
+        revokeRole(DEFAULT_ADMIN_ROLE, oldDefaultAdmin);
     }
 
     function submitTransferUntrustless(uint eventId, CommonStructs.Transfer[] calldata transfers) public onlyRole(RELAY_ROLE) whenNotPaused {
