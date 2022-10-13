6a27b6ff2b36d0f8186e78bff2794a6442532213
nacknime-official <nacknime.official@gmail.com>
2022-10-13 13:39:15 +0300

    fix(relay/mpc): convert ws to http for getting full msg

--- relay/pkg/mpc/networking/client/client.go
+++ relay/pkg/mpc/networking/client/client.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 
@@ -71,7 +72,7 @@ func (s *Client) SetFullMsg(fullMsg []byte) {
 }
 
 func (s *Client) GetFullMsg() ([]byte, error) {
-	resp, err := s.httpClient.Get(s.serverURL + common.EndpointFullMsg)
+	resp, err := s.httpClient.Get(wsToHttpUrl(s.serverURL) + common.EndpointFullMsg)
 	if err != nil {
 		return nil, err
 	}
@@ -216,4 +217,13 @@ func (s *Client) stopOperation() {
 	s.Lock()
 	defer s.Unlock()
 	s.operation = nil
+}
+
+func wsToHttpUrl(url string) string {
+	if strings.HasPrefix(url, "wss") {
+		return "https" + url[3:]
+	} else if strings.HasPrefix(url, "ws") {
+		return "http" + url[2:]
+	}
+	return url
 }
