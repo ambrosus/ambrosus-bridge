1904d8ae740dea57c322df3af7836974a7fd86fd
svinerus <svinerus@gmail.com>
2022-11-21 02:59:47 +0200

    Prepare to upgrade ETH_AmbBridge to UntrustlessMpc

--- contracts/deploy/03_ETH_AmbBridge.ts
+++ contracts/deploy/03_ETH_AmbBridge.ts
@@ -2,6 +2,7 @@ import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {DeployFunction} from "hardhat-deploy/types";
 import {ethers} from "hardhat";
 import {addNewTokensToBridge, options, parseNet, readConfig_, setSideBridgeAddress} from "./utils/utils";
+import {getAddresses} from "./utils/prod_addresses";
 
 const BRIDGE_NAME = "ETH_AmbBridge";
 
@@ -18,18 +19,30 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       timeframeSeconds: isMainNet ? 60 * 60 * 4 : 60,
       lockTime: isMainNet ? 60 * 10 : 60,
       minSafetyBlocks: 10,
-    },
-    [ // untrusless1 params:
-      5, // threshold
-      [  // multisigners
+    }, []
+  )
+
+  // upgrade to untrustless-mpc; set watchdogs and fee_provider roles; remove DEFAULT_ADMIN_ROLE from deployer
+  let {owner} = await hre.getNamedAccounts();
+  const prod_addresses = getAddresses(BRIDGE_NAME);
+  deployOptions.proxy.execute.onUpgrade = {
+    methodName: "upgrade",
+    args: [
+      [ // watchdogs (user-relays + master-relay + admin)
         "0x260cfE305cA40CaE1a32Ba7611137eF4d7146233", // Kevin
         "0xEB1c6a8a84063B1cef8B9a23AB87Bf926035A21a", // Lang
         "0x40B7d71E70fA6311cB0b300c1Ba6926A2A9000b8", // Rory
         "0xb017DcCC473499C83f1b553bE564f3CeAf002254", // Andrey
-        "0x0f071e1785e3E115360E04c9C8D53e958E6f85FE", // Master
-      ]
-    ],
-  )
+
+        isMainNet ? prod_addresses.relayAddress : owner, // Master relay
+        isMainNet ? prod_addresses.adminAddress : owner, // Admin
+      ],
+      isMainNet ? prod_addresses.relayAddress : owner, // fee_provider (relay)
+      isMainNet ? prod_addresses.adminAddress : owner, // remove DEFAULT_ADMIN_ROLE from this address
+      'TODO' // new mpc relay address
+      // todo remove RELAY_ROLE from old relay address
+    ]
+  };
 
 
   if (isMainNet) {
