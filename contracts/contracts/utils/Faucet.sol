2a5e7f084afae570bca8a505f338fed66934091b
svin <svinerus@gmail.com>
2023-01-10 16:04:25 +0200

    Add `chainId` field to `Faucet` event

--- contracts/contracts/utils/Faucet.sol
+++ contracts/contracts/utils/Faucet.sol
@@ -4,16 +4,16 @@ pragma solidity 0.8.6;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract Faucet is AccessControl {
-    event Faucet(address indexed to, uint256 indexed eventId, uint256 amount);
+    event Faucet(address indexed to, uint256 indexed eventId, uint256 indexed chainId, uint256 amount);
 
     constructor(address[] memory admins) payable {
         for (uint i = 0; i < admins.length; i++)
             _setupRole(DEFAULT_ADMIN_ROLE, admins[i]);
     }
 
-    function faucet(address toAddress, uint256 eventId, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function faucet(address toAddress, uint256 eventId, uint256 chainId, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
         withdraw(toAddress, amount);
-        emit Faucet(toAddress, eventId, amount);
+        emit Faucet(toAddress, eventId, chainId, amount);
     }
 
     function withdraw(address toAddress, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
